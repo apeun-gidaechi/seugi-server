bb6ccb1b45f2adf97ab53277ab49e1d816571a2f
이슬아 <rheeseula@gmail.com>
2024-03-20 21:30:39 +0900

    Feat :: Member 도메인 CreateMemberController 추가 (예외 처리 x)

---
+++ src/main/kotlin/seugi/server/domain/member/adapter/in/controller/CreateMemberController.kt
@@ -0,0 +1,21 @@
+package seugi.server.domain.member.adapter.`in`.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import seugi.server.domain.member.adapter.`in`.dto.CreateMemberDTO
+import seugi.server.domain.member.port.`in`.CreateMemberUseCase
+import seugi.server.global.response.BaseResponse
+
+@RestController
+@RequestMapping("member")
+class CreateMemberController (
+    private val createMemberUseCase: CreateMemberUseCase
+) {
+
+    @PostMapping("create")
+    fun createMember(memberDTO: CreateMemberDTO): BaseResponse<Any> {
+        return createMemberUseCase.createMember(memberDTO)
+    }
+
+}
--- src/main/kotlin/seugi/server/domain/member/application/service/CreateMemberService.kt
+++ src/main/kotlin/seugi/server/domain/member/application/service/CreateMemberService.kt
@@ -5,16 +5,24 @@ import seugi.server.domain.member.adapter.`in`.dto.CreateMemberDTO
 import seugi.server.domain.member.application.model.Member
 import seugi.server.domain.member.port.`in`.CreateMemberUseCase
 import seugi.server.domain.member.port.out.SaveMemberPort
+import seugi.server.global.response.BaseResponse
 
 @Service
 class CreateMemberService (
     private val saveMemberPort: SaveMemberPort
 ): CreateMemberUseCase {
 
-    override fun createMember(memberDTO: CreateMemberDTO) {
+    override fun createMember(memberDTO: CreateMemberDTO): BaseResponse<Any> {
         saveMemberPort.saveMember(Member(
             null
         ))
+
+        return BaseResponse (
+            code = 200,
+            success = true,
+            message = "회원가입 성공 !!",
+            data = emptyList()
+        )
     }
 
 }
