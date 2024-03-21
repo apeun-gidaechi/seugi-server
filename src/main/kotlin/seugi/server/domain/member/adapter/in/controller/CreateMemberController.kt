a05e256979c5ea87a7c7b50f1ef0bd4008ef1ac4
이슬아 <rheeseula@gmail.com>
2024-03-21 13:26:02 +0900

    Fix :: Member 도메인 CreateMemberController @RequestBody 애노테이션 추가

--- src/main/kotlin/seugi/server/domain/member/adapter/in/controller/CreateMemberController.kt
+++ src/main/kotlin/seugi/server/domain/member/adapter/in/controller/CreateMemberController.kt
@@ -1,6 +1,7 @@
 package seugi.server.domain.member.adapter.`in`.controller
 
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import seugi.server.domain.member.adapter.`in`.dto.CreateMemberDTO
@@ -14,7 +15,7 @@ class CreateMemberController (
 ) {
 
     @PostMapping("create")
-    fun createMember(memberDTO: CreateMemberDTO): BaseResponse<Any> {
+    fun createMember(@RequestBody memberDTO: CreateMemberDTO): BaseResponse<Any> {
         return createMemberUseCase.createMember(memberDTO)
     }
 
