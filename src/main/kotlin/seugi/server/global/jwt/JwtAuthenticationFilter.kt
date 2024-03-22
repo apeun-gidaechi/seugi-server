6f5a47063e96f875bb75ac3755cc8428f7737c63
이슬아 <rheeseula@gmail.com>
2024-03-22 12:44:09 +0900

    Feat :: JWT JwtAuthenticationFilter 추가

---
+++ src/main/kotlin/seugi/server/global/jwt/JwtAuthenticationFilter.kt
@@ -0,0 +1,35 @@
+package seugi.server.global.jwt
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+
+class JwtAuthenticationFilter(
+    private val jwtUtils: JwtUtils
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        var token: String? = request.getHeader("Authorization")
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = jwtUtils.getToken(token)
+
+            if (jwtUtils.isExpired(token)) {
+                val authentication: Authentication = jwtUtils.getAuthentication(token)
+
+                println(authentication)
+
+                SecurityContextHolder.getContext().authentication = authentication
+            }
+        }
+
+        doFilter(request, response, filterChain)
+    }
+}
