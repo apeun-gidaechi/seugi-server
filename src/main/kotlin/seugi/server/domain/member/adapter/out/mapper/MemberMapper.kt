f2272247e3797906a056d720a317813be9fec75e
이슬아 <rheeseula@gmail.com>
2024-03-22 11:38:32 +0900

    Feat :: Member 도메인 Mapper 메서드 추가 (Entity -> Member)

--- src/main/kotlin/seugi/server/domain/member/adapter/out/mapper/MemberMapper.kt
+++ src/main/kotlin/seugi/server/domain/member/adapter/out/mapper/MemberMapper.kt
@@ -3,17 +3,27 @@ package seugi.server.domain.member.adapter.out.mapper
 import org.springframework.stereotype.Component
 import seugi.server.domain.member.adapter.out.entity.MemberEntity
 import seugi.server.domain.member.application.model.Member
+import seugi.server.domain.member.application.model.value.*
 
 @Component
 class MemberMapper {
-
     fun returnMemberToMemberEntityWithoutId(member: Member): MemberEntity {
         return MemberEntity(
-            id = null,
             name = member.name.value,
             email = member.email.value,
             password = member.password.value,
-            birth = member.birth.value
+            birth = member.birth.value,
+        )
+    }
+
+    fun returnMemberEntityToMember(memberEntity: MemberEntity): Member {
+        return Member (
+            id = MemberId(memberEntity.id),
+            name = MemberName(memberEntity.name),
+            email = MemberEmail(memberEntity.email),
+            password = MemberPassword(memberEntity.password),
+            birth = MemberBirth(memberEntity.birth),
+            role = MemberRole(memberEntity.role)
         )
     }
 
