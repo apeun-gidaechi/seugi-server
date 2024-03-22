5226cdd9c1f03f38b6b5bc1ac8ebafebde4b4457
이슬아 <rheeseula@gmail.com>
2024-03-22 12:44:31 +0900

    Feat :: Global SecurityConfig JwtAuthenticationFilter 추가 (예외 처리 X)

--- src/main/kotlin/seugi/server/global/config/SecurityConfig.kt
+++ src/main/kotlin/seugi/server/global/config/SecurityConfig.kt
@@ -2,16 +2,20 @@ package seugi.server.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import seugi.server.global.jwt.JwtAuthenticationFilter
+import seugi.server.global.jwt.JwtUtils
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(
-    private val bCryptPasswordEncoder: BCryptPasswordEncoder
+class SecurityConfig (
+    private val jwtUtils: JwtUtils
 ) {
 
     @Bean
@@ -32,9 +36,12 @@ class SecurityConfig(
                 )
             }
             .authorizeHttpRequests {
-                it.requestMatchers("").permitAll()
+                it.requestMatchers("/member/**").permitAll()
                     .anyRequest().authenticated()
             }
+
+            .addFilterBefore(JwtAuthenticationFilter(jwtUtils), UsernamePasswordAuthenticationFilter::class.java)
+
             .build()
 
     }
