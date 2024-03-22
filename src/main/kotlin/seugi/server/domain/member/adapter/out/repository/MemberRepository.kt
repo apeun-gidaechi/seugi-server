370df7b9c45d2a341790c6536616a93b22f44657
이슬아 <rheeseula@gmail.com>
2024-03-22 11:38:11 +0900

    Feat :: Member 도메인 Repository 메서드 추가 (2개)

--- src/main/kotlin/seugi/server/domain/member/adapter/out/repository/MemberRepository.kt
+++ src/main/kotlin/seugi/server/domain/member/adapter/out/repository/MemberRepository.kt
@@ -6,4 +6,9 @@ import seugi.server.domain.member.adapter.out.entity.MemberEntity
 
 @Repository
 interface MemberRepository: JpaRepository<MemberEntity, Long> {
+
+    fun findByEmail(email: String): MemberEntity
+
+    fun existsByEmail(email: String): Boolean
+
 }
