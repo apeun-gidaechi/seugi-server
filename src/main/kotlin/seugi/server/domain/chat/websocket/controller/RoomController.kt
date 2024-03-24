b61210d58ed090e322b8f6d1227d5902866029fd
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-24 15:48:19 +0900

    Fix :: MVC 패턴에서 Rest 방식으로 수정

--- src/main/kotlin/seugi/server/domain/chat/websocket/controller/RoomController.kt
+++ src/main/kotlin/seugi/server/domain/chat/websocket/controller/RoomController.kt
@@ -1,13 +1,9 @@
 package seugi.server.domain.chat.websocket.controller
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import seugi.server.domain.chat.websocket.dto.ChatRoomDTO
 import seugi.server.domain.chat.websocket.repository.ChatRoomRepository
 
 @Controller
@@ -17,25 +13,20 @@ class RoomController (
 ){
     //채팅방 목록 조회
     @GetMapping("/rooms")
-    fun rooms() : ModelAndView{
-        val mv : ModelAndView = ModelAndView("chat/rooms")
-        mv.addObject("list", repository.findAllRooms())
-
-        return mv
+    fun rooms() : List<ChatRoomDTO?> {
+        return repository.findAllRooms()
     }
 
     //채팅방 개설
     @PostMapping("/room")
-    fun create(@RequestParam name:String, rttr : RedirectAttributes) : String{
-        println("name : $name")
-        rttr.addFlashAttribute("roomName", repository.createChatRoomDTO(name))
-        return "redirect:/chat/rooms"
+    fun create(@RequestParam name:String) : ChatRoomDTO{
+        val chatRoomDTO = repository.createChatRoomDTO(name)
+        return chatRoomDTO
     }
 
     //채팅방 조회
-    @GetMapping("/room")
-    fun getRoom(roomId: String , model:Model) {
-        println("room id : $roomId")
-        model.addAttribute("room", repository.findRoomById(roomId))
+    @GetMapping("/room/{id}")
+    fun getRoom(@PathVariable id: String): ChatRoomDTO? {
+        return repository.findRoomById(id)
     }
 }
