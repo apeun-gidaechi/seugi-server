9affa188897afac51e6dc8844efaf6e106919654
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-25 20:27:23 +0900

    Fix :: 메시지 형식 기본값 수정

--- src/main/kotlin/seugi/server/domain/chat/websocket/dto/ChatMessageDto.kt
+++ src/main/kotlin/seugi/server/domain/chat/websocket/dto/ChatMessageDto.kt
@@ -3,5 +3,7 @@ package seugi.server.domain.chat.websocket.dto
 data class ChatMessageDto(
     var roomId : String,
     var writer : String,
-    var message : String
+    var message : String,
+    var emoji : Long,
+    var time  : String
 )
