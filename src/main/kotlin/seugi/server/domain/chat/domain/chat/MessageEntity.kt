0bb9dea8d51b49bc24a0e27180ee91e3c3214be8
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-26 23:17:03 +0900

    Fix :: 메시지 형식 확일화에 따른 수정 #4
    Embeddable 을 활용하여 읽은사람 안읽은사람 추가

--- src/main/kotlin/seugi/server/domain/chat/domain/chat/MessageEntity.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/chat/MessageEntity.kt
@@ -1,7 +1,9 @@
 package seugi.server.domain.chat.domain.chat
 
 import jakarta.persistence.*
-import seugi.server.domain.chat.domain.chat.embeddable.emoji
+import seugi.server.domain.chat.domain.chat.embeddable.Emoji
+import seugi.server.domain.chat.domain.chat.embeddable.Read
+import seugi.server.domain.chat.domain.chat.embeddable.UnRead
 import seugi.server.domain.chat.domain.room.ChatRoomEntity
 import seugi.server.domain.chat.domain.status.ChatStatusEnum
 import java.time.LocalDateTime
@@ -21,17 +23,20 @@ class MessageEntity {
     val message:String = ""
 
     @ElementCollection
-    var emoji:List<emoji> = emptyList()
+    var emoji:MutableList<Emoji> = mutableListOf()
 
     @Column(nullable = false)
     val timestamp : LocalDateTime = LocalDateTime.now()
 
+    @ElementCollection
+    var read: MutableList<Read> = mutableListOf()
+
+    @ElementCollection
+    var unRead: MutableList<UnRead> = mutableListOf()
+
     @Enumerated(value = EnumType.STRING)
     var messageStatus: ChatStatusEnum = ChatStatusEnum.ALIVE
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_room_id")
-    var chatRoom: ChatRoomEntity? = null
 
 
 }
