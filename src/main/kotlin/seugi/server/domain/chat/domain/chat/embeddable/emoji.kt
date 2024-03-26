2298e64cf254afdf0aef453c17797ba07b7166ba
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-26 21:58:07 +0900

    Feat :: 채팅 이모지 #4

---
+++ src/main/kotlin/seugi/server/domain/chat/domain/chat/embeddable/emoji.kt
@@ -0,0 +1,12 @@
+package seugi.server.domain.chat.domain.chat.embeddable
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+class emoji {
+    @Column(name = "emoji_id")
+    var emojiId: Long? = null
+    @Column(name = "emoji_set_user")
+    var emojiUserId: Long? = null
+}
