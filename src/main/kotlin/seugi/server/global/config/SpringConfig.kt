66aa0c5fb38277e44905fd79682b8365b44c8d5f
이슬아 <rheeseula@gmail.com>
2024-03-27 19:58:55 +0900

    Feat :: Global SpringConfig에서 ObjectMapper를 빈으로 등록하도록 함

--- src/main/kotlin/seugi/server/global/config/SpringConfig.kt
+++ src/main/kotlin/seugi/server/global/config/SpringConfig.kt
@@ -1,5 +1,6 @@
 package seugi.server.global.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -10,6 +11,11 @@ class SpringConfig {
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
     }
 
 }
