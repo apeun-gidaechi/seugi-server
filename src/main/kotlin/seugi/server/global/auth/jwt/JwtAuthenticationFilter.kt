e54172f56d3c6330c4ffb563f7491cb8fc888a30
이슬아 <rheeseula@gmail.com>
2024-03-27 20:01:57 +0900

    Feat :: JWT JwtAuthenticationFilter에서 CustomException 사용

--- src/main/kotlin/seugi/server/global/auth/jwt/JwtAuthenticationFilter.kt
+++ src/main/kotlin/seugi/server/global/auth/jwt/JwtAuthenticationFilter.kt
@@ -1,14 +1,19 @@
 package seugi.server.global.auth.jwt
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.ExpiredJwtException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+import seugi.server.global.exception.CustomErrorCode
+import seugi.server.global.response.BaseResponse
 
 class JwtAuthenticationFilter(
-    private val jwtUtils: JwtUtils
+    private val jwtUtils: JwtUtils,
+    private val objectMapper: ObjectMapper
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
@@ -21,15 +26,37 @@ class JwtAuthenticationFilter(
         if (token != null && token.startsWith("Bearer ")) {
             token = jwtUtils.getToken(token)
 
-            if (jwtUtils.isExpired(token)) {
-                val authentication: Authentication = jwtUtils.getAuthentication(token)
-
-                println(authentication)
-
-                SecurityContextHolder.getContext().authentication = authentication
+            try  {
+                jwtUtils.isExpired(token)
+            } catch (e: ExpiredJwtException) {
+                setErrorResponse(response, CustomErrorCode.JWT_TOKEN_EXPIRED)
             }
+
+            val authentication: Authentication = jwtUtils.getAuthentication(token)
+
+            SecurityContextHolder.getContext().authentication = authentication
         }
 
         doFilter(request, response, filterChain)
+    }
+
+    private fun setErrorResponse(
+        response: HttpServletResponse,
+        errorCode: CustomErrorCode
+    ) {
+        response.status = errorCode.code
+        response.contentType = "application/json"
+
+        response.writer.write(
+            objectMapper.writeValueAsString(
+                BaseResponse<String> (
+                    code = errorCode.code,
+                    success = false,
+                    message = errorCode.msg,
+                    data = emptyList()
+                )
+            )
+        )
+
     }
 }
