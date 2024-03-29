9267824c7cba022db20c15ea32368f699034565f
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-29 23:16:19 +0900

    Fix :: ChatRoom Domain 형식 수정

--- src/main/kotlin/seugi/server/domain/chat/domain/room/model/Room.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/room/model/Room.kt
@@ -1,14 +1,13 @@
 package seugi.server.domain.chat.domain.room.model
 
-import seugi.server.domain.chat.domain.chat.MessageEntity
-import seugi.server.domain.chat.domain.joined.JoinedEntity
+import seugi.server.domain.chat.domain.joined.model.Joined
 import seugi.server.domain.chat.domain.status.ChatStatusEnum
 import java.time.LocalDateTime
 
 data class Room(
-    val id: Long,
+    val id: Long? = null,
     val chatName: String,
-    val createdAt: LocalDateTime,
-    val chatStatusEnum: ChatStatusEnum,
-    val joined: MutableList<JoinedEntity>
+    val createdAt: LocalDateTime? = null,
+    val chatStatusEnum: ChatStatusEnum? = null,
+    val joined: MutableList<Joined>
 )
