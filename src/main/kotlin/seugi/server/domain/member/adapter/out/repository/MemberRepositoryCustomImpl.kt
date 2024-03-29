168cb94e8320dcd1a1176ab0a6d944b22054544f
이슬아 <rheeseula@gmail.com>
2024-03-29 22:42:18 +0900

    Feat :: MemberRepositoryCustom QueryDSL로 구현

---
+++ src/main/kotlin/seugi/server/domain/member/adapter/out/repository/MemberRepositoryCustomImpl.kt
@@ -0,0 +1,36 @@
+package seugi.server.domain.member.adapter.out.repository
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.stereotype.Repository
+import seugi.server.domain.member.adapter.out.entity.MemberEntity
+import seugi.server.domain.member.adapter.out.entity.QMemberEntity
+import java.util.*
+
+@Repository
+class MemberRepositoryCustomImpl (
+    private val jpaQueryFactory: JPAQueryFactory
+) : MemberRepositoryCustom {
+
+    override fun findByEmail(email: String): Optional<MemberEntity> {
+        val memberEntity: QMemberEntity = QMemberEntity.memberEntity
+
+        val result = jpaQueryFactory
+            .select(memberEntity)
+            .from(memberEntity)
+            .where(memberEntity.email.eq(email))
+            .fetchOne()
+
+        return Optional.ofNullable(result)
+    }
+
+    override fun existsByEmail(email: String): Boolean {
+        val memberEntity: QMemberEntity = QMemberEntity.memberEntity
+
+        return jpaQueryFactory
+            .select(memberEntity)
+            .from(memberEntity)
+            .where(memberEntity.email.eq(email))
+            .fetchOne() != null
+    }
+
+}
