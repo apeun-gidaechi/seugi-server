6ed39daffe3745ffec0836172a5e120548def225
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-30 01:50:16 +0900

    Feat :: ChatRoomMapper 구체화 #4

--- src/main/kotlin/seugi/server/domain/chat/domain/room/mapper/RoomMapper.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/room/mapper/RoomMapper.kt
@@ -1,8 +1,11 @@
 package seugi.server.domain.chat.domain.room.mapper
 
 import org.springframework.stereotype.Component
+import seugi.server.domain.chat.domain.joined.JoinedEntity
+import seugi.server.domain.chat.domain.joined.model.Joined
 import seugi.server.domain.chat.domain.room.ChatRoomEntity
 import seugi.server.domain.chat.domain.room.model.Room
+import seugi.server.domain.chat.presentation.room.dto.request.CreateRoomRequest
 import seugi.server.global.common.Mapper
 
 @Component
@@ -12,16 +15,38 @@ class RoomMapper: Mapper<Room, ChatRoomEntity> {
             id = entity.id!!,
             chatName = entity.chatName,
             createdAt = entity.createdAt,
-            joined = entity.joined,
+            joined = entity.joined.map { toJoinedDomain(it) }.toMutableList(),
             chatStatusEnum = entity.chatStatus,
         )
     }
 
     override fun toEntity(domain: Room): ChatRoomEntity {
-        return ChatRoomEntity(
+        val chatRoomEntity = ChatRoomEntity(
             chatName = domain.chatName,
-            joined = domain.joined
+        )
+        chatRoomEntity.joined = domain.joined.map { doEntity(it, chatRoomEntity) }.toMutableList()
 
+        return chatRoomEntity
+    }
+
+    fun toRoom(createRoomRequest: CreateRoomRequest) : Room{
+        return Room(
+            chatName = createRoomRequest.roomName,
+            joined = createRoomRequest.joinUsers.toMutableList()
+        )
+    }
+
+    private fun toJoinedDomain(joined: JoinedEntity) : Joined{
+        return Joined(
+            joinUserId = joined.joinedUserId!!,
+            chatRoomId = joined.chatRoom?.id!!
+        )
+    }
+
+    private fun doEntity(joined: Joined, chatRoomEntity: ChatRoomEntity) : JoinedEntity{
+        return JoinedEntity(
+            joinedUserId = joined.joinUserId,
+            chatRoom = chatRoomEntity
         )
     }
 }
