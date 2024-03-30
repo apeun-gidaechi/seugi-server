f5b0ff1fa45b2139f128930c18781619c8ac483e
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-31 01:38:45 +0900

    Feat :: Joined 로직 구현 #4
    채팅방 생성시 유저 참여 저장 로직을 위한 서비스 로직

--- src/main/kotlin/seugi/server/domain/chat/application/service/joined/JoinedServiceImpl.kt
+++ src/main/kotlin/seugi/server/domain/chat/application/service/joined/JoinedServiceImpl.kt
@@ -2,9 +2,21 @@ package seugi.server.domain.chat.application.service.joined
 
 import org.springframework.stereotype.Service
 import seugi.server.domain.chat.domain.joined.JoinedRepository
+import seugi.server.domain.chat.domain.joined.mapper.JoinedMapper
 
 @Service
 class JoinedServiceImpl(
-    private val joinedRepository: JoinedRepository
+    private val joinedRepository: JoinedRepository,
+    private val joinedMapper: JoinedMapper
 ) : JoinedService {
+
+    override fun joinUserJoined(chatRoomId: Long, joinedUserId: List<Long>) {
+        joinedRepository.saveAll(joinedUserId.map {
+            joinedMapper.toEntity(
+                joinedUserId = it,
+                chatRoomId = chatRoomId
+            ) }
+        )
+    }
+
 }
