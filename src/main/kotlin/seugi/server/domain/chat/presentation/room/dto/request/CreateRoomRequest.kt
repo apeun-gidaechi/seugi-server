211155a8cdf36402bcdb64bde6072211b7df3afd
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-31 01:39:30 +0900

    Fix :: MongoDB로 바끼며 채팅방 생성 리퀘스트 수정 #4

--- src/main/kotlin/seugi/server/domain/chat/presentation/room/dto/request/CreateRoomRequest.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/room/dto/request/CreateRoomRequest.kt
@@ -1,8 +1,6 @@
 package seugi.server.domain.chat.presentation.room.dto.request
 
-import seugi.server.domain.chat.domain.joined.model.Joined
-
 data class CreateRoomRequest(
     val roomName : String,
-    val joinUsers : List<Joined>
+    val joinUsers : List<Long>
 )
