46919831cabff9cd6fc74a03f14af1b476452015
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-30 20:25:20 +0900

    Build :: Redis 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     kapt("jakarta.annotation:jakarta.annotation-api")
     kapt("jakarta.persistence:jakarta.persistence-api")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 
