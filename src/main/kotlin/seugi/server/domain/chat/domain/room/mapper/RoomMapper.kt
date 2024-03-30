6f10aae2ff101f47a80f269067420b2e31ddb9a2
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-31 01:13:55 +0900

    Fix :: MongoDB 로 바꾸면서 형태 수정 #4

--- src/main/kotlin/seugi/server/domain/chat/domain/room/mapper/RoomMapper.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/room/mapper/RoomMapper.kt
@@ -2,7 +2,6 @@ package seugi.server.domain.chat.domain.room.mapper
 
 import org.springframework.stereotype.Component
 import seugi.server.domain.chat.domain.joined.JoinedEntity
-import seugi.server.domain.chat.domain.joined.model.Joined
 import seugi.server.domain.chat.domain.room.ChatRoomEntity
 import seugi.server.domain.chat.domain.room.model.Room
 import seugi.server.domain.chat.presentation.room.dto.request.CreateRoomRequest
@@ -15,38 +14,19 @@ class RoomMapper: Mapper<Room, ChatRoomEntity> {
             id = entity.id!!,
             chatName = entity.chatName,
             createdAt = entity.createdAt,
-            joined = entity.joined.map { toJoinedDomain(it) }.toMutableList(),
             chatStatusEnum = entity.chatStatus,
         )
     }
 
     override fun toEntity(domain: Room): ChatRoomEntity {
-        val chatRoomEntity = ChatRoomEntity(
-            chatName = domain.chatName,
+        return ChatRoomEntity(
+            chatName = domain.chatName
         )
-        chatRoomEntity.joined = domain.joined.map { doEntity(it, chatRoomEntity) }.toMutableList()
-
-        return chatRoomEntity
     }
 
     fun toRoom(createRoomRequest: CreateRoomRequest) : Room{
         return Room(
-            chatName = createRoomRequest.roomName,
-            joined = createRoomRequest.joinUsers.toMutableList()
-        )
-    }
-
-    private fun toJoinedDomain(joined: JoinedEntity) : Joined{
-        return Joined(
-            joinUserId = joined.joinedUserId!!,
-            chatRoomId = joined.chatRoom?.id!!
-        )
-    }
-
-    private fun doEntity(joined: Joined, chatRoomEntity: ChatRoomEntity) : JoinedEntity{
-        return JoinedEntity(
-            joinedUserId = joined.joinUserId,
-            chatRoom = chatRoomEntity
+            chatName = createRoomRequest.roomName
         )
     }
 }
