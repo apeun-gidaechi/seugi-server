f908d506fbb8eaac3c9e6fa995e343e4c78b0d5c
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-31 01:13:34 +0900

    Fix :: MongoDB 로 바꾸면서 형태 수정 #4

--- src/main/kotlin/seugi/server/domain/chat/domain/chat/embeddable/Emoji.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/chat/embeddable/Emoji.kt
@@ -1,12 +1,9 @@
 package seugi.server.domain.chat.domain.chat.embeddable
 
-import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
 
 @Embeddable
-class Emoji {
-    @Column(name = "emoji_id")
-    var emojiId: Long? = null
-    @Column(name = "emoji_set_user")
-    var emojiUserId: Long? = null
-}
+class Emoji(
+    var userId: Long,
+    var emojiId: Long
+)
--- src/main/kotlin/seugi/server/domain/chat/domain/room/model/Room.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/room/model/Room.kt
@@ -1,6 +1,5 @@
 package seugi.server.domain.chat.domain.room.model
 
-import seugi.server.domain.chat.domain.joined.model.Joined
 import seugi.server.domain.chat.domain.status.ChatStatusEnum
 import java.time.LocalDateTime
 
@@ -8,6 +7,5 @@ data class Room(
     val id: Long? = null,
     val chatName: String,
     val createdAt: LocalDateTime? = null,
-    val chatStatusEnum: ChatStatusEnum? = null,
-    val joined: MutableList<Joined>
+    val chatStatusEnum: ChatStatusEnum? = null
 )
