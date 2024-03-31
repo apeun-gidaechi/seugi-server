609369bb609297575e83b3175c6c84edf7375653
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-03-31 21:04:44 +0900

    Fix :: 유저 인증 반환 코드 null 로 돌아오던 문제 해결 #4

--- src/main/kotlin/seugi/server/global/common/annotation/GetAuthenticatedId.kt
+++ src/main/kotlin/seugi/server/global/common/annotation/GetAuthenticatedId.kt
@@ -4,5 +4,5 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.VALUE_PARAMETER)
-@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? -1 : #this.id")
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? -1 : #this.id.value")
 annotation class GetAuthenticatedId
