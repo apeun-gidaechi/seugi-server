912917b565d7ce1ce1d435bdbb7474ef7583b025
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 23:51:25 +0900

    Feat :: 채팅 저장 JPA #4

--- src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedRepository.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedRepository.kt
@@ -3,5 +3,6 @@ package seugi.server.domain.chat.domain.joined
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface JoinedRepository : MongoRepository<JoinedEntity, Long>{
+    fun findByChatRoomId(chatRoomId: Long) : JoinedEntity
     fun findByJoinedUserIdEquals(joinedUserId: Long) : List<JoinedEntity>
 }
