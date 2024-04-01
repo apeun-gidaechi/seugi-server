45243e685e738897be6b020a38c8f073c22065f7
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 09:33:27 +0900

    Feat :: 자신이 속한 방 찾기 로직에서 조회를 위한 JPA 인터페이스 작성 #4

--- src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedRepository.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedRepository.kt
@@ -1,7 +1,7 @@
 package seugi.server.domain.chat.domain.joined
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.mongodb.repository.MongoRepository
 
-interface JoinedRepository : CrudRepository<JoinedEntity, Long>{
+interface JoinedRepository : MongoRepository<JoinedEntity, Long>{
     fun findByJoinedUserIdEquals(joinedUserId: Long) : List<JoinedEntity>
 }
