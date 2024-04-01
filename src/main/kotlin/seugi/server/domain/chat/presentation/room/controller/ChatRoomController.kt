a37846434a59d45d2cd74914f537270abfb19373
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 09:15:34 +0900

    Feat :: 채팅방 생성 및 자신이 속한 채팅방 불러오기 로직 #4

--- src/main/kotlin/seugi/server/domain/chat/presentation/room/controller/ChatRoomController.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/room/controller/ChatRoomController.kt
@@ -1,11 +1,10 @@
 package seugi.server.domain.chat.presentation.room.controller
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import seugi.server.domain.chat.application.service.room.ChatRoomService
+import seugi.server.domain.chat.domain.room.model.Room
 import seugi.server.domain.chat.presentation.room.dto.request.CreateRoomRequest
+import seugi.server.global.common.annotation.GetAuthenticatedId
 import seugi.server.global.response.BaseResponse
 
 @RestController
@@ -16,9 +15,18 @@ class ChatRoomController(
 
     @PostMapping("/create")
     fun createRoom(
-        //@GetAuthenticatedId id: Long,
+        @GetAuthenticatedId id: Long,
         @RequestBody createRoomRequest: CreateRoomRequest
     ) : BaseResponse<Unit>{
-        return chatRoomService.createChatRoom(createRoomRequest)
+        return chatRoomService.createChatRoom(createRoomRequest, id)
     }
+
+    @GetMapping("/search")
+    fun searchRooms(
+        @GetAuthenticatedId userid: Long
+    ) : BaseResponse<List<Room>>{
+        return chatRoomService.searchRooms(userid)
+    }
+
+
 }
