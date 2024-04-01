3682b25e9e69ddb8f04fbeb3dab5ae7d861d6fa6
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 23:51:13 +0900

    Feat :: Stomp 커넥션시 토큰에 유저 정보 빼서 헤더에 넣어주기 #4

--- src/main/kotlin/seugi/server/domain/chat/presentation/websocket/config/StompWebSocketConfig.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/websocket/config/StompWebSocketConfig.kt
@@ -1,18 +1,31 @@
 package seugi.server.domain.chat.presentation.websocket.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.config.annotation.*
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.ChannelInterceptor
+import org.springframework.messaging.support.MessageBuilder
+import org.springframework.messaging.support.MessageHeaderAccessor
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import seugi.server.global.auth.jwt.JwtUserDetails
+import seugi.server.global.auth.jwt.JwtUtils
 
 @Configuration
 @EnableWebSocketMessageBroker
-class StompWebSocketConfig : WebSocketMessageBrokerConfigurer {
-
+class StompWebSocketConfig(
+    private val jwtUtils: JwtUtils
+) : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/stomp/chat")
             .setAllowedOrigins("*")
-//            .withSockJS()
     }
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
@@ -20,4 +33,34 @@ class StompWebSocketConfig : WebSocketMessageBrokerConfigurer {
         config.setApplicationDestinationPrefixes("/pub")
     }
 
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(object : ChannelInterceptor {
+            override fun preSend(message: Message<*>, channel: MessageChannel): Message<*> {
+                val accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor::class.java)!!
+
+                if (accessor.command == StompCommand.CONNECT) {
+                    val authToken = accessor.getNativeHeader("Authorization")?.firstOrNull()
+                    if (authToken != null && authToken.startsWith("Bearer ")) {
+                        val token = authToken.removePrefix("Bearer ")
+                        val auth = jwtUtils.getAuthentication(token)
+                        SecurityContextHolder.getContext().authentication = auth
+
+                        val userId:String = (auth.principal as JwtUserDetails).id?.value.toString()
+                        accessor.setLeaveMutable(true)
+                        accessor.setHeader("user-id", userId)
+                        return MessageBuilder.createMessage(message.payload, accessor.messageHeaders)
+                    }
+                } else if (accessor.command == StompCommand.SEND) {
+                    val auth = SecurityContextHolder.getContext().authentication
+                    val userId:String = (auth.principal as JwtUserDetails).id?.value.toString()
+
+                    accessor.setLeaveMutable(true)
+                    accessor.setHeader("user-id", userId)
+                    return MessageBuilder.createMessage(message.payload, accessor.messageHeaders)
+                }
+
+                return message
+            }
+        })
+    }
 }
