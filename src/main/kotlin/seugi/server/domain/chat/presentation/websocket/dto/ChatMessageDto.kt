f0aef915463b3bb4f718ba808db0771057402152
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 20:31:28 +0900

    Refactor :: Timestamp 자동설정으로 변경 #4

--- src/main/kotlin/seugi/server/domain/chat/presentation/websocket/dto/ChatMessageDto.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/websocket/dto/ChatMessageDto.kt
@@ -1,8 +1,7 @@
 package seugi.server.domain.chat.presentation.websocket.dto
 
 data class ChatMessageDto(
-    var roomId : String,
+    var roomId : Long,
     var writer : String,
-    var message : String,
-    var timestamp  : String
+    var message : String
 )
