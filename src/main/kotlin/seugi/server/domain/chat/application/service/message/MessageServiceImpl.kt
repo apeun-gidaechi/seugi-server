99ec3f7e2045140d97c856f1fbee318e49bb100c
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 20:33:02 +0900

    Feat :: Message 저장 로직 추가

--- src/main/kotlin/seugi/server/domain/chat/application/service/message/MessageServiceImpl.kt
+++ src/main/kotlin/seugi/server/domain/chat/application/service/message/MessageServiceImpl.kt
@@ -2,10 +2,32 @@ package seugi.server.domain.chat.application.service.message
 
 import org.springframework.stereotype.Service
 import seugi.server.domain.chat.domain.chat.MessageRepository
+import seugi.server.domain.chat.domain.chat.mapper.MessageMapper
+import seugi.server.domain.chat.domain.joined.JoinedRepository
+import seugi.server.domain.chat.presentation.websocket.dto.ChatMessageDto
+import seugi.server.domain.member.adapter.out.repository.MemberRepository
 
 @Service
 class MessageServiceImpl(
-    private val messageRepository: MessageRepository
+    private val messageRepository: MessageRepository,
+    private val memberRepository: MemberRepository,
+    private val messageMapper: MessageMapper,
+    private val joinedRepository: JoinedRepository
 ) : MessageService {
+
+    override fun saveMessage(chatMessageDto: ChatMessageDto, userId: Long){
+
+        val joinedEntity = joinedRepository.findById(chatMessageDto.roomId)
+        messageRepository.save(
+            messageMapper.toEntity(
+                messageMapper.toMessage(
+                    chatMessageDto = chatMessageDto,
+                    joinedEntity = joinedEntity.get(),
+                    userId = userId,
+                    writer = memberRepository.findById(userId).get().name
+                )
+            )
+        )
+    }
 
 }
--- src/main/kotlin/seugi/server/domain/chat/presentation/websocket/controller/StompChatController.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/websocket/controller/StompChatController.kt
@@ -3,20 +3,21 @@ package seugi.server.domain.chat.presentation.websocket.controller
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Controller
+import seugi.server.domain.chat.application.service.message.MessageService
 import seugi.server.domain.chat.presentation.websocket.dto.ChatMessageDto
+import seugi.server.global.common.annotation.GetAuthenticatedId
 
 @Controller
 class StompChatController(
-    val template: SimpMessagingTemplate
+    val template: SimpMessagingTemplate,
+    private val messageService: MessageService
 ) {
-    @MessageMapping("/chat/enter")
-    fun enter(message: ChatMessageDto) {
-        message.message = message.writer + "입장함"
-        template.convertAndSend("/sub/chat/room/" + message.roomId, message)
-    }
-
     @MessageMapping("/chat/message")
-    fun message(message: ChatMessageDto){
+    fun message(
+        @GetAuthenticatedId id: Long,
+        message: ChatMessageDto
+    ){
         template.convertAndSend("/sub/chat/room/" + message.roomId, message)
+        messageService.saveMessage(message, id)
     }
 }
