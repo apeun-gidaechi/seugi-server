157a02a1ca35583f5fd6d9cac89913ed6449cbd1
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-01 20:32:44 +0900

    Refactor :: Message 모델링 하며 id 값을 자동 생성으로 둔 뒤 chatRoomId 를 따로 빼는 형식으로 수정 #4

--- src/main/kotlin/seugi/server/domain/chat/domain/chat/model/Message.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/chat/model/Message.kt
@@ -1,17 +1,17 @@
 package seugi.server.domain.chat.domain.chat.model
 
 import seugi.server.domain.chat.domain.chat.embeddable.Emoji
-import seugi.server.domain.chat.domain.chat.embeddable.Read
-import seugi.server.domain.chat.domain.chat.embeddable.UnRead
-import java.time.LocalDateTime
+import seugi.server.domain.chat.domain.status.ChatStatusEnum
 
 data class Message(
+    val id : String? = null,
     val chatRoomId: Long,
     val writer: String,
+    val userId: Long,
     val message: String,
-    val emoji: List<Emoji>,
-    val timestamp: String?,
-    val read: List<Read>,
-    val unRead: List<UnRead>,
-    val messageStatus: String
+    val emoji: List<Emoji> = emptyList(),
+    val timestamp: String? = null,
+    val read: List<Long> = emptyList(),
+    val unRead: List<Long>,
+    val messageStatus: ChatStatusEnum = ChatStatusEnum.ALIVE
 )
