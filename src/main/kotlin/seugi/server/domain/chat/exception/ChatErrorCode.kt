d06c6e37c46c49d6653f096bd91da41188ef374d
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-02 19:29:33 +0900

    Feat :: 채팅방에 권한이 없을때 조회시 에러 코드 추가 #32

--- src/main/kotlin/seugi/server/domain/chat/exception/ChatErrorCode.kt
+++ src/main/kotlin/seugi/server/domain/chat/exception/ChatErrorCode.kt
@@ -9,6 +9,7 @@ enum class ChatErrorCode (
     override val message: String,
 ): CustomErrorCode {
 
-    CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "C2", "채팅방을 찾을 수 없습니다.")
+    CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "C2", "채팅방을 찾을 수 없습니다."),
+    NO_ACCESS_ROOM(HttpStatus.BAD_REQUEST, "C3", "채팅방 접근 권한이 없습니다.")
 
 }
