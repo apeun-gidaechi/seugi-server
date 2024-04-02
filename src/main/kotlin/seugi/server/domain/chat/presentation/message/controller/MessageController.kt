05ec3b3b075160b06701aa364d87bab82c27487c
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-02 19:31:23 +0900

    Feat :: MessageController 역할 주석 추가

--- src/main/kotlin/seugi/server/domain/chat/presentation/message/controller/MessageController.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/message/controller/MessageController.kt
@@ -3,8 +3,14 @@ package seugi.server.domain.chat.presentation.message.controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * 메시지 삭제, 이모지 달기, 읽음표시
+ */
 
 @RestController
-@RequestMapping
-class MessageController {
+@RequestMapping("/message")
+class MessageController(
+
+) {
+
 }
