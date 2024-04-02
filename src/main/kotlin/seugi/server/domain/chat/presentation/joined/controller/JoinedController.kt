7bc8f9d9377d010a93e7d1fcdc6b20c926e405de
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-02 19:31:18 +0900

    Feat :: JoinedController 역할 주석 추가

--- src/main/kotlin/seugi/server/domain/chat/presentation/joined/controller/JoinedController.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/joined/controller/JoinedController.kt
@@ -3,7 +3,14 @@ package seugi.server.domain.chat.presentation.joined.controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * 참가자 추가, 삭제
+ */
+
+
 @RestController
 @RequestMapping
-class JoinedController {
+class JoinedController(
+
+) {
 }
