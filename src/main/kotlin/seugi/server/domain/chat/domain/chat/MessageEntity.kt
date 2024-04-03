c2dacb0c5927f0e22009025249fa2958ddbe59a8
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-03 11:49:40 +0900

    Fix :: 요구사항에 맞게 메시지 엔티티 수정 #32

--- src/main/kotlin/seugi/server/domain/chat/domain/chat/MessageEntity.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/chat/MessageEntity.kt
@@ -1,27 +1,38 @@
 package seugi.server.domain.chat.domain.chat
 
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
 import seugi.server.domain.chat.domain.chat.embeddable.Emoji
+import seugi.server.domain.chat.domain.chat.embeddable.MessageUserId
+import seugi.server.domain.chat.domain.chat.model.Type
 import seugi.server.domain.chat.domain.status.ChatStatusEnum
+import seugi.server.domain.member.adapter.out.entity.MemberEntity
 import java.time.LocalDateTime
 
 
 @Document(collection = "messages")
-class MessageEntity(
+data class MessageEntity(
 
     @Id
-    val id: String? = null,
+    val id: ObjectId? = null,
 
     var chatRoomId: Long? = null,
 
-    var writer: String,
+    @Enumerated(EnumType.ORDINAL)
+    val type : Type,
 
-    var userId: Long,
+    var author : MemberEntity,
 
-    var message: String,
+    val message : String = "",
 
-    var emoji: MutableList<Emoji> = mutableListOf(),
+    val emojiList: MutableList<Emoji> = MutableList(8) { Emoji(it+1) },
+
+    val mention : List<MessageUserId> = emptyList(),
+
+    val mentionALl : Boolean = false,
 
     var timestamp: LocalDateTime = LocalDateTime.now(),
 
