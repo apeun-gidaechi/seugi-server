fae767c292adecb9c996fb0e19538f3896f4881d
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-04 17:52:48 +0900

    Fix :: JoinedEntity 형식 변경 #32

--- src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedEntity.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedEntity.kt
@@ -2,6 +2,7 @@ package seugi.server.domain.chat.domain.joined
 
 import com.querydsl.core.annotations.QueryEntity
 import jakarta.persistence.*
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
 
 
@@ -9,6 +10,8 @@ import org.springframework.data.mongodb.core.mapping.Document
 @QueryEntity
 class JoinedEntity (
     @Id
+    val id:ObjectId? = null,
+
     val chatRoomId: Long? = null,
 
     var joinedUserId : MutableSet<Long> = emptyArray<Long>().toMutableSet(),
