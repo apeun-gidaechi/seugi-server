2b6c509d5aaf7f45e279b915a56a839ce2ecc9eb
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-04 17:53:04 +0900

    Fix :: JoinedEntity 형식 변경 #32

--- src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedRepository.kt
+++ src/main/kotlin/seugi/server/domain/chat/domain/joined/JoinedRepository.kt
@@ -1,8 +1,9 @@
 package seugi.server.domain.chat.domain.joined
 
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface JoinedRepository : MongoRepository<JoinedEntity, Long>{
+interface JoinedRepository : MongoRepository<JoinedEntity, ObjectId>{
     fun findByChatRoomId(chatRoomId: Long) : JoinedEntity
     fun findByJoinedUserIdEquals(joinedUserId: Long) : List<JoinedEntity>
 }
