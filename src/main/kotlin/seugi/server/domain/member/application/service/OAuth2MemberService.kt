66822199eaf4b4d0aa19eb078f2f8498a440d353
이슬아 <rheeseula@gmail.com>
2024-04-06 20:25:53 +0900

    Feat :: Member 도메인 OAuth2 액세스 토큰 발급

--- src/main/kotlin/seugi/server/domain/member/application/service/OAuth2MemberService.kt
+++ src/main/kotlin/seugi/server/domain/member/application/service/OAuth2MemberService.kt
@@ -1,4 +1,51 @@
 package seugi.server.domain.member.application.service
 
-class OAuth2MemberService {
+import com.fasterxml.jackson.databind.JsonNode
+import jakarta.transaction.Transactional
+import org.springframework.http.*
+import org.springframework.stereotype.Service
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+import seugi.server.domain.member.port.`in`.OAuth2MemberUseCase
+import seugi.server.global.auth.oauth.OAuth2Properties
+
+@Service
+@Transactional
+class OAuth2MemberService (
+    private val oAuth2Properties: OAuth2Properties,
+    private val restTemplate: RestTemplate
+) : OAuth2MemberUseCase {
+
+    override fun process(code: String, registrationId: String) {
+        val token = getAccessToken(code)
+
+        println(token)
+    }
+
+    override fun getAccessToken(code: String): String {
+        val params: MultiValueMap<String, String> = LinkedMultiValueMap()
+        params.add("code", code)
+        params.add("client_id", oAuth2Properties.clientId)
+        params.add("client_secret", oAuth2Properties.clientSecret)
+        params.add("redirect_uri", oAuth2Properties.redirectURI)
+        params.add("grant_type", "authorization_code")
+
+        val headers: HttpHeaders = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        val entity: HttpEntity<*> = HttpEntity<Any?>(params, headers)
+
+        val responseNode: ResponseEntity<JsonNode> = restTemplate.exchange(
+            oAuth2Properties.tokenURI,
+            HttpMethod.POST,
+            entity,
+            JsonNode::class.java
+        )
+
+        val accessTokenNode = responseNode.body
+
+        return accessTokenNode!!.get("access_token").asText()
+    }
+
 }
