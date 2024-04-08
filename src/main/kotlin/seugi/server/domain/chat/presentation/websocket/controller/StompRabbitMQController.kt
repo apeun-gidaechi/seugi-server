597187089e96057ee7ccee70ef3bcfbab97638b7
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-08 13:54:06 +0900

    Feat :: STOMP -> RabbitMQ 이전 채팅저장 로직 추가 #36

--- src/main/kotlin/seugi/server/domain/chat/presentation/websocket/controller/StompRabbitMQController.kt
+++ src/main/kotlin/seugi/server/domain/chat/presentation/websocket/controller/StompRabbitMQController.kt
@@ -2,17 +2,28 @@ package seugi.server.domain.chat.presentation.websocket.controller
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpAttributesContextHolder
 import org.springframework.stereotype.Controller
+import seugi.server.domain.chat.application.service.message.MessageService
+import seugi.server.domain.chat.domain.chat.model.Message
 import seugi.server.domain.chat.presentation.websocket.dto.ChatMessageDto
 
 
 @Controller
 class StompRabbitMQController(
-    private val template: RabbitTemplate
+    private val template: RabbitTemplate,
+    private val messageService: MessageService
 ) {
 
     @MessageMapping("chat.message")
     fun send(chat: ChatMessageDto) {
-        template.convertAndSend("chat.exchange", "room.${chat.roomId}", chat)
+
+        val simpAttributes = SimpAttributesContextHolder.currentAttributes()
+        val userId = simpAttributes.getAttribute("user-id") as String?
+        if (userId != null) {
+            val domainMessage: Message = messageService.saveMessage(chat, userId.toLong())
+            template.convertAndSend("chat.exchange", "room.${chat.roomId}", domainMessage)
+        }
+
     }
 }
