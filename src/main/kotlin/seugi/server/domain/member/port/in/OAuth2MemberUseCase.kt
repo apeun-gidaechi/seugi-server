68114b4631f5e38efb415122ceb4d78cfbf2771a
이슬아 <rheeseula@gmail.com>
2024-04-09 18:02:53 +0900

    Fix :: Member 도메인 OAuth2 process 메서드 반환 수정
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/seugi/server/domain/member/port/in/OAuth2MemberUseCase.kt
+++ src/main/kotlin/seugi/server/domain/member/port/in/OAuth2MemberUseCase.kt
@@ -1,10 +1,13 @@
 package seugi.server.domain.member.port.`in`
 
 import com.fasterxml.jackson.databind.JsonNode
+import seugi.server.global.auth.jwt.JwtInfo
+import seugi.server.global.auth.jwt.JwtUtils
+import seugi.server.global.response.BaseResponse
 
 interface OAuth2MemberUseCase {
 
-    fun process(code: String, registrationId: String)
+    fun process(code: String, registrationId: String): BaseResponse<JwtInfo>
 
     fun getAccessToken(code: String): String
 
