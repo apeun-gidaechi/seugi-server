038ef851a149cf8e3144cf1c81524d9837aff2d9
이슬아 <rheeseula@gmail.com>
2024-04-13 13:48:16 +0900

    Feat :: Member 도메인 OAuth2 컨트롤러 complete 라우트 추가
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/seugi/server/domain/member/adapter/in/controller/OAuth2MemberController.kt
+++ src/main/kotlin/seugi/server/domain/member/adapter/in/controller/OAuth2MemberController.kt
@@ -2,8 +2,10 @@ package seugi.server.domain.member.adapter.`in`.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import seugi.server.domain.member.adapter.`in`.dto.OAuth2MemberDTO
 import seugi.server.domain.member.port.`in`.OAuth2MemberUseCase
 import seugi.server.global.auth.jwt.JwtInfo
 import seugi.server.global.response.BaseResponse
@@ -15,8 +17,13 @@ class OAuth2MemberController (
 ) {
 
     @PostMapping("/oauth2")
-    fun socialLogin(code: String, registrationId: String): BaseResponse<JwtInfo> {
-        return oAuth2MemberUseCase.process(code, registrationId)
+    fun socialLogin(code: String, provider: String): BaseResponse<JwtInfo> {
+        return oAuth2MemberUseCase.process(code, provider)
+    }
+
+    @PostMapping("/oauth2/complete")
+    fun setDetails(@RequestBody dto: OAuth2MemberDTO): BaseResponse<Unit> {
+        return oAuth2MemberUseCase.complete(dto)
     }
 
 }
