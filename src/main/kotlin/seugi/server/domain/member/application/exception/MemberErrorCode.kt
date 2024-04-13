73286f052127e0f95e86d5db55971a3703f61b51
이슬아 <rheeseula@gmail.com>
2024-04-13 13:47:06 +0900

    Feat :: Member 도메인 ErrorCode MEMBER_ALREADY_SUFFICIENT 추가
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/seugi/server/domain/member/application/exception/MemberErrorCode.kt
+++ src/main/kotlin/seugi/server/domain/member/application/exception/MemberErrorCode.kt
@@ -11,6 +11,7 @@ enum class MemberErrorCode (
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER-001", "멤버를 찾을 수 없네"),
     MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "MEMBER-002", "멤버가 이미 존재한다"),
-    MEMBER_NOT_SUFFICIENT(HttpStatus.PARTIAL_CONTENT, "MEMBER-003", "멤버의 정보가 부족하다")
+    MEMBER_NOT_SUFFICIENT(HttpStatus.PARTIAL_CONTENT, "MEMBER-003", "멤버의 정보가 부족하다"),
+    MEMBER_ALREADY_SUFFICIENT(HttpStatus.ALREADY_REPORTED, "MEMBER-004", "가입이 완료된 계정")
 
 }
