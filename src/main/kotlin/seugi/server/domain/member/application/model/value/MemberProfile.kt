ae3fafaf0211ed96db76b37a87cd7c13c3491903
이슬아 <rheeseula@gmail.com>
2024-04-13 13:39:33 +0900

    Fix :: Member 도메인 모델 name, birth, profile 필드 가변으로 설정
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/seugi/server/domain/member/application/model/value/MemberProfile.kt
+++ src/main/kotlin/seugi/server/domain/member/application/model/value/MemberProfile.kt
@@ -5,12 +5,12 @@ import jakarta.persistence.Embeddable
 @Embeddable
 data class MemberProfile (
 
-    val status: String = "", // 상태메시지
-    val nick: String = "", // 닉네임
-    val spot: String = "", // 직위
-    val belong: String = "", // 소속
-    val phone: String = "", // 휴대전화번호
-    val wire: String = "", // 유선전화번호
-    val location: String = "", // 근무위치
+    var status: String = "", // 상태메시지
+    var nick: String = "", // 닉네임
+    var spot: String = "", // 직위
+    var belong: String = "", // 소속
+    var phone: String = "", // 휴대전화번호
+    var wire: String = "", // 유선전화번호
+    var location: String = "", // 근무위치
 
 )
