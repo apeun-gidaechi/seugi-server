05811172a24107e1a20258808b48f9c087295439
이슬아 <rheeseula@gmail.com>
2024-04-14 20:43:58 +0900

    Fix :: JWT_MEMBER_NOT_EXIST 에러 코드 추가

--- src/main/kotlin/com/seugi/api/global/auth/jwt/exception/JwtErrorCode.kt
+++ src/main/kotlin/com/seugi/api/global/auth/jwt/exception/JwtErrorCode.kt
@@ -12,5 +12,6 @@ enum class JwtErrorCode (
     JWT_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "J1", "토큰 만료다"),
     JWT_MEMBER_NOT_MATCH(HttpStatus.UNAUTHORIZED, "J2", "비밀번호 일치 노노"),
     JWT_NULL_EXCEPTION(HttpStatus.SERVICE_UNAVAILABLE, "J3", "어떻게 지평좌표계로 고정을 하셨죠?"),
+    JWT_MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "J4", "멤버가 존재하지 않아요")
 
 }
