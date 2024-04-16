6dbb1b22ffb5a77306bf6c1b3877cc2986919ef3
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-16 19:28:37 +0900

    Fix :: S3 Properties 추가 #52

--- src/main/kotlin/com/seugi/api/global/auth/oauth/S3Properties.kt
+++ src/main/kotlin/com/seugi/api/global/auth/oauth/S3Properties.kt
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Component
 @Component
 class S3Properties (
     @Value("\${aws.s3.accessKey}") val accessKey: String,
-    @Value("\${aws.s3.secretKey}") val secretKey: String
+    @Value("\${aws.s3.secretKey}") val secretKey: String,
+    @Value("\${aws.s3.bucket}") val bucket: String
 )
