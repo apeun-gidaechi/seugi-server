8bd318cadbace7452e0681cfda382c00ff13496d
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-16 17:22:33 +0900

    Feat :: S3 기능 추가 #52

---
+++ src/main/kotlin/com/seugi/api/global/infra/S3.kt
@@ -0,0 +1,46 @@
+package com.seugi.api.global.infra
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.seugi.api.global.auth.oauth.S3Properties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.multipart.MultipartFile
+
+@Configuration
+class S3(
+    val s3Properties: S3Properties
+) {
+    @Bean
+    fun amazonS3Client(): AmazonS3 {
+        return AmazonS3ClientBuilder.standard()
+            .withCredentials(
+                AWSStaticCredentialsProvider(BasicAWSCredentials(s3Properties.accessKey, s3Properties.secretKey))
+            )
+            .withRegion(Regions.AP_NORTHEAST_2)
+            .build()
+    }
+
+    fun uploadMultipleFile(
+        @RequestPart file: MultipartFile,
+    ) {
+        val objectMetadata = ObjectMetadata().apply {
+            this.contentType = file.contentType
+            this.contentLength = file.size
+        }
+        val putObjectRequest = PutObjectRequest(
+            "bucketName",
+            "objectKey",
+            file.inputStream,
+            objectMetadata,
+        )
+
+        amazonS3Client().putObject(putObjectRequest)
+    }
+}
