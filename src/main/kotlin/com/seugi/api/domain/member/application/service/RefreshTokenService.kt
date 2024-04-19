304fe5f23f12622bcbeea96d05bb9f15b07fb760
이슬아 <rheeseula@gmail.com>
2024-04-19 20:59:06 +0900

    Member 도메인 RefreshTokenService 구현

--- src/main/kotlin/com/seugi/api/domain/member/application/service/RefreshTokenService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/RefreshTokenService.kt
@@ -1,4 +1,25 @@
 package com.seugi.api.domain.member.application.service
 
-class RefreshTokenService {
+import com.seugi.api.domain.member.port.`in`.RefreshTokenUseCase
+import com.seugi.api.global.auth.jwt.JwtInfo
+import com.seugi.api.global.auth.jwt.JwtUtils
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+
+@Service
+class RefreshTokenService (
+    private val jwtUtils: JwtUtils
+) : RefreshTokenUseCase {
+
+    override fun refreshToken(token: String): BaseResponse<JwtInfo> {
+        return BaseResponse (
+            HttpStatus.OK.value(),
+            true,
+            "OK",
+            "리프레시 성공 !",
+            jwtUtils.refreshToken(token)
+        )
+    }
+
 }
