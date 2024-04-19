86eb14ac3d15b97d72e0dd47f0a1b5ccbc81524a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-20 00:01:08 +0900

    Refactor :: 안쓰는 import 제거 #68

--- src/main/kotlin/com/seugi/api/domain/workspace/domain/entity/WorkspaceEntity.kt
+++ src/main/kotlin/com/seugi/api/domain/workspace/domain/entity/WorkspaceEntity.kt
@@ -1,7 +1,6 @@
 package com.seugi.api.domain.workspace.domain.entity
 
 import com.seugi.api.domain.workspace.domain.enums.Status
-import com.seugi.api.domain.workspace.domain.model.Workspace
 import jakarta.persistence.*
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
