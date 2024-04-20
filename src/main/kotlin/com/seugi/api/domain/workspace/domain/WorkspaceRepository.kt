d6c6bc52c437467edb515477fb82e99e6c3b11ab
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-20 21:10:13 +0900

    Fix :: 자신이 참가한 워크스페이스 불러올때 삭제된 워크스페이스도 불러오던거 수정 #68

--- src/main/kotlin/com/seugi/api/domain/workspace/domain/WorkspaceRepository.kt
+++ src/main/kotlin/com/seugi/api/domain/workspace/domain/WorkspaceRepository.kt
@@ -4,13 +4,16 @@ import com.seugi.api.domain.workspace.domain.entity.WorkspaceEntity
 import com.seugi.api.domain.workspace.domain.enums.Status
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.mongodb.repository.Query
-import org.springframework.data.repository.query.Param
 
 interface WorkspaceRepository: MongoRepository<WorkspaceEntity, ObjectId> {
 
-    @Query("SELECT w FROM Workspace w WHERE ?1 == w.student or ?1 == w.teacher or ?1 == w.workspaceAdmin or ?1 == w.middleAdmin And w.status == ALIVE")
-    fun findByUserIdAndStatus(@Param("userId") userId: Long, @Param("status") status: Status): List<WorkspaceEntity>
+    fun findByStudentEqualsOrTeacherEqualsOrMiddleAdminEqualsOrWorkspaceAdminEqualsAndStatus(
+        student: Long,
+        teacher: Long,
+        middleAdmin: Long,
+        workspaceAdmin: Long,
+        status: Status
+    ): List<WorkspaceEntity>
     fun findByWorkspaceCodeEquals(workspaceCode: String) : WorkspaceEntity?
     fun existsByWorkspaceCode(workspaceCode: String) : Boolean
 }
