31a9e526fae192ef4867e18a21c2191da45ba2dc
이슬아 <rheeseula@gmail.com>
2024-04-20 21:00:23 +0900

    Feat :: Member 도메인 RefershTokenController 작성

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/RefreshTokenController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/RefreshTokenController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/member")
-class RefreshMemberController (
+class RefreshTokenController (
     private val refreshTokenUseCase: RefreshTokenUseCase
 ) {
 
