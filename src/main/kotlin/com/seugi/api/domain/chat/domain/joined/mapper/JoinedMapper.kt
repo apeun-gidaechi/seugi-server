63e5eca017adbab0b8efc2c73d3584475a7471a2
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-22 18:16:45 +0900

    Fix :: Joined 모델 워크스페이스 아이디 포함하도록 수정 #72

--- src/main/kotlin/com/seugi/api/domain/chat/domain/joined/mapper/JoinedMapper.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/domain/joined/mapper/JoinedMapper.kt
@@ -13,7 +13,8 @@ class JoinedMapper : Mapper<Joined, JoinedEntity> {
             joinUserId = entity.joinedUserId,
             roomType = entity.roomType,
             roomAdmin = entity.roomAdmin,
-            chatRoomId = entity.chatRoomId!!
+            chatRoomId = entity.chatRoomId!!,
+            workspaceId = entity.workspaceID
         )
     }
 
@@ -22,16 +23,18 @@ class JoinedMapper : Mapper<Joined, JoinedEntity> {
             joinedUserId = domain.joinUserId,
             roomType = domain.roomType,
             roomAdmin = domain.roomAdmin,
-            chatRoomId = domain.chatRoomId
+            chatRoomId = domain.chatRoomId,
+            workspaceID = domain.workspaceId
         )
     }
 
-    fun toEntity(chatRoomId : Long, joinedUserId : List<Long>, type: RoomType, roomAdmin: Long) : JoinedEntity{
+    fun toEntity(chatRoomId : Long, joinedUserId : List<Long>, type: RoomType, roomAdmin: Long, workspaceId: String) : JoinedEntity{
         return JoinedEntity(
             joinedUserId = joinedUserId.toMutableSet(),
             roomType = type,
             roomAdmin = roomAdmin,
-            chatRoomId = chatRoomId
+            chatRoomId = chatRoomId,
+            workspaceID = workspaceId
         )
     }
 }
