c49ecae2d420460ed09586af27a4e079e8a32f4a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-22 18:17:23 +0900

    Fix :: CreateRequest 에 워크스페이스 ID 추가 #72

--- src/main/kotlin/com/seugi/api/domain/chat/presentation/room/dto/request/CreateRoomRequest.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/presentation/room/dto/request/CreateRoomRequest.kt
@@ -2,6 +2,7 @@ package com.seugi.api.domain.chat.presentation.room.dto.request
 
 data class CreateRoomRequest(
     var roomName : String = "",
+    val workspaceId: String,
     val joinUsers : MutableList<Long>? = emptyArray<Long>().toMutableList(),
     val chatRoomImg : String
 )
