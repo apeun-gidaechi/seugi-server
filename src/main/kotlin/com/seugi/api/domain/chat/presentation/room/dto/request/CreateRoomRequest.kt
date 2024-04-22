838d2998445516100e6d1853ded6783e6770fee7
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-22 19:36:08 +0900

    Fix :: Json 직렬화 문제 해결 #72
    기본값 설정 안해서 직렬화 못하던 문제 해결

--- src/main/kotlin/com/seugi/api/domain/chat/presentation/room/dto/request/CreateRoomRequest.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/presentation/room/dto/request/CreateRoomRequest.kt
@@ -2,7 +2,7 @@ package com.seugi.api.domain.chat.presentation.room.dto.request
 
 data class CreateRoomRequest(
     var roomName : String = "",
-    val workspaceId: String,
+    val workspaceId: String = "",
     val joinUsers : MutableList<Long>? = emptyArray<Long>().toMutableList(),
-    val chatRoomImg : String
+    val chatRoomImg : String = ""
 )
