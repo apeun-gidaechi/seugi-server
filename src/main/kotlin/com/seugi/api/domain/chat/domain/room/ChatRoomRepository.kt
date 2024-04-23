dd672dc5857671e5acca6051ca9dcfc0c4c53a11
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-04-23 21:54:41 +0900

    Feat :: ChatRoomRepository 에 유저 검색로직 추가 #72

--- src/main/kotlin/com/seugi/api/domain/chat/domain/room/ChatRoomRepository.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/domain/room/ChatRoomRepository.kt
@@ -1,5 +1,8 @@
 package com.seugi.api.domain.chat.domain.room
 
+import com.seugi.api.domain.chat.domain.room.model.Room
 import org.springframework.data.repository.CrudRepository
 
-interface ChatRoomRepository : CrudRepository <ChatRoomEntity, Long> , ChatRoomRepositoryCustom
+interface ChatRoomRepository : CrudRepository <ChatRoomEntity, Long> , ChatRoomRepositoryCustom{
+    override fun searchRoom(input: String): List<Room>
+}
--- src/main/kotlin/com/seugi/api/domain/chat/domain/room/ChatRoomRepositoryCustom.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/domain/room/ChatRoomRepositoryCustom.kt
@@ -1,4 +1,7 @@
 package com.seugi.api.domain.chat.domain.room
 
+import com.seugi.api.domain.chat.domain.room.model.Room
+
 interface ChatRoomRepositoryCustom {
+    fun searchRoom(input: String): List<Room>
 }
