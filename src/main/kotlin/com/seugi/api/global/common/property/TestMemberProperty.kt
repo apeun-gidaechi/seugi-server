21264835daf595bf3c1ec80902be02f6b6b71db1
이슬아 <rheeseula@gmail.com>
2024-04-27 17:45:17 +0900

    Feat :: Global TestMemberProperty 작성

--- src/main/kotlin/com/seugi/api/global/common/property/TestMemberProperty.kt
+++ src/main/kotlin/com/seugi/api/global/common/property/TestMemberProperty.kt
@@ -1,4 +1,4 @@
-package com.seugi.api.domain.member.application.property
+package com.seugi.api.global.common.property
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Component
 @Component
 class TestMemberProperty (
     @Value("\${test.email}") val email: String,
-    @Value("\${test.password}") val password: String,
-    )
+    @Value("\${test.password}") val password: String
+)
