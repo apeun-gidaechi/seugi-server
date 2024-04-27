55e9ca66fe9d8395e566ed55712e89057ceb4cfb
이슬아 <rheeseula@gmail.com>
2024-04-27 17:38:50 +0900

    Feat :: 테스트 토큰 발급용 컨트롤러 작성

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/TestMemberController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/TestMemberController.kt
@@ -1,2 +1,31 @@
 package com.seugi.api.domain.member.adapter.`in`.controller
 
+import com.seugi.api.domain.member.adapter.`in`.dto.LoginMemberDTO
+import com.seugi.api.domain.member.port.`in`.LoginMemberUseCase
+import com.seugi.api.global.auth.jwt.JwtInfo
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/member")
+class TestMemberController (
+    @Value("\${test.email}") val email: String,
+    @Value("\${test.password}") val password: String,
+
+    private val loginMemberUseCase: LoginMemberUseCase
+) {
+
+    @PostMapping("/test")
+    fun testLogin(): BaseResponse<JwtInfo> {
+        return loginMemberUseCase.loginMember(
+            LoginMemberDTO(
+                email,
+                password
+            )
+        )
+    }
+
+}
