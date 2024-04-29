7792757f82cc32aa2a80333e65ce45d2dc0548bd
이슬아 <rheeseula@gmail.com>
2024-04-29 19:24:02 +0900

    Fix :: RefreshTokenUseCase 반환 타입 String으로 변경

--- src/main/kotlin/com/seugi/api/domain/member/port/in/RefreshTokenUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/port/in/RefreshTokenUseCase.kt
@@ -1,10 +1,9 @@
 package com.seugi.api.domain.member.port.`in`
 
-import com.seugi.api.global.auth.jwt.JwtInfo
 import com.seugi.api.global.response.BaseResponse
 
 interface RefreshTokenUseCase {
 
-    fun refreshToken(token: String): BaseResponse<JwtInfo>
+    fun refreshToken(token: String): BaseResponse<String>
 
 }
