642ae14358b672ae29e6302bee4016bc968a0dd6
이슬아 <rheeseula@gmail.com>
2024-05-04 23:17:43 +0900

    Feat :: Member 도메인 RefreshTokenService 토큰 확인
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/service/RefreshTokenService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/RefreshTokenService.kt
@@ -1,5 +1,6 @@
 package com.seugi.api.domain.member.application.service
 
+import com.seugi.api.domain.member.adapter.out.repository.TokenRepository
 import com.seugi.api.domain.member.application.model.value.MemberRefreshToken
 import com.seugi.api.domain.member.port.`in`.RefreshTokenUseCase
 import com.seugi.api.domain.member.port.out.LoadMemberPort
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service
 class RefreshTokenService (
     private val jwtUtils: JwtUtils,
     private val loadMemberPort: LoadMemberPort,
+    private val tokenRepository: TokenRepository
 ) : RefreshTokenUseCase {
 
     override fun refreshToken(token: String): BaseResponse<String> {
@@ -25,6 +27,9 @@ class RefreshTokenService (
             )
         )
 
+        if (!tokenRepository.loadToken(token).isPresent) {
+           throw CustomException(JwtErrorCode.JWT_TOKEN_NOT_VALID)
+        }
 
         return BaseResponse (
             HttpStatus.OK.value(),
