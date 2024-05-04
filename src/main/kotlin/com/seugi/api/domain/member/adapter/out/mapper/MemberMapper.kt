48a65345d3226d0a9fcd763a9a6ba4fa5ae81d48
이슬아 <rheeseula@gmail.com>
2024-05-04 19:45:36 +0900

    Fix :: Member 도메인 Mapper ProfileEntity 반영

--- src/main/kotlin/com/seugi/api/domain/member/adapter/out/mapper/MemberMapper.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/out/mapper/MemberMapper.kt
@@ -2,6 +2,7 @@ package com.seugi.api.domain.member.adapter.out.mapper
 
 import org.springframework.stereotype.Component
 import com.seugi.api.domain.member.adapter.out.entity.MemberEntity
+import com.seugi.api.domain.member.adapter.out.entity.MemberProfileEntity
 import com.seugi.api.domain.member.application.model.Member
 import com.seugi.api.domain.member.application.model.value.*
 import com.seugi.api.global.common.Mapper
@@ -20,12 +21,26 @@ class MemberMapper: Mapper<Member, MemberEntity> {
             role = MemberRole(entity.role),
             loginId = MemberLoginId(entity.loginId),
             provider = MemberProvider(entity.provider),
-            providerId = MemberProviderId(entity.providerId)
+            providerId = MemberProviderId(entity.providerId),
+            profiles = entity.profiles.map {
+                MemberProfile (
+                    it.id,
+                    it.memberId,
+                    it.workspaceId,
+                    it.status,
+                    it.nick,
+                    it.spot,
+                    it.belong,
+                    it.phone,
+                    it.wire,
+                    it.location
+                )
+            }
         )
     }
 
     override fun toEntity(domain: Member): MemberEntity {
-        return MemberEntity(
+        return MemberEntity (
             id = domain.id?.value ?: 0,
             name = domain.name.value,
             email = domain.email.value,
@@ -34,7 +49,21 @@ class MemberMapper: Mapper<Member, MemberEntity> {
             birth = domain.birth.value,
             loginId = domain.loginId.value,
             provider = domain.provider.value,
-            providerId = domain.providerId.value
+            providerId = domain.providerId.value,
+            profiles = domain.profiles.map {
+                MemberProfileEntity (
+                    it.id,
+                    it.memberId,
+                    it.workspaceId,
+                    it.status,
+                    it.nick,
+                    it.spot,
+                    it.belong,
+                    it.phone,
+                    it.wire,
+                    it.location
+                )
+            }
         )
     }
 
