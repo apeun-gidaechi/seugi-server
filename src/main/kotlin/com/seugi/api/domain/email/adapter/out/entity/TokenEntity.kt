03ed341d7890b8c01e6b79f928442fe9eaf472c7
이슬아 <rheeseula@gmail.com>
2024-05-05 11:21:42 +0900

    Feat :: Email 도메인 TokenEntity 추가

--- src/main/kotlin/com/seugi/api/domain/email/adapter/out/entity/TokenEntity.kt
+++ src/main/kotlin/com/seugi/api/domain/email/adapter/out/entity/TokenEntity.kt
@@ -1,4 +1,13 @@
 package com.seugi.api.domain.email.adapter.out.entity
 
-class TokenEntity {
-}
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+
+@RedisHash("token")
+class TokenEntity (
+
+    @Id
+    val token: String,
+    val email: String
+
+)
