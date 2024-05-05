9b818b9ce12a66a6c1ca67bd1634db6b6addeea8
이슬아 <rheeseula@gmail.com>
2024-05-05 16:17:50 +0900

    Fix :: Member 도메인 Controller 포트 위치 변경 반영

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/EditMemberController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/EditMemberController.kt
@@ -1,10 +1,13 @@
 package com.seugi.api.domain.member.adapter.`in`.controller
 
-import com.seugi.api.domain.member.port.`in`.EditMemberUseCase
+import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
+import com.seugi.api.domain.member.application.port.`in`.EditMemberUseCase
 import com.seugi.api.global.common.annotation.GetAuthenticatedId
 import com.seugi.api.global.response.BaseResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -16,6 +19,14 @@ class EditMemberController (
     @GetMapping("/picture")
     fun editPicture(url: String, @GetAuthenticatedId id: Long) : BaseResponse<Unit> {
         return editMemberUseCase.editPicture(url, id)
+    }
+
+    @PostMapping("/profile/{workspaceId}")
+    fun editProfile(dto: EditProfileDTO,
+                    @PathVariable workspaceId: String,
+                    @GetAuthenticatedId id: Long
+    ) {
+        return editMemberUseCase.editProfile(dto, workspaceId, id)
     }
 
 }
--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/LoginMemberController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/LoginMemberController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import com.seugi.api.domain.member.adapter.`in`.dto.LoginMemberDTO
-import com.seugi.api.domain.member.port.`in`.LoginMemberUseCase
+import com.seugi.api.domain.member.application.port.`in`.LoginMemberUseCase
 import com.seugi.api.global.auth.jwt.JwtInfo
 import com.seugi.api.global.response.BaseResponse
 
