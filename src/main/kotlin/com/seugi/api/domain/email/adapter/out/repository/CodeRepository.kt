23328232d99d2e59623d3f8aa1d7b2713a30a9f5
이슬아 <rheeseula@gmail.com>
2024-05-06 11:22:09 +0900

    Feat :: Email 도메인 CodeRepository 추가

--- src/main/kotlin/com/seugi/api/domain/email/adapter/out/repository/CodeRepository.kt
+++ src/main/kotlin/com/seugi/api/domain/email/adapter/out/repository/CodeRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EmailRepository: CrudRepository<CodeEntity, String> {
+interface CodeRepository: CrudRepository<CodeEntity, String> {
 
 }
