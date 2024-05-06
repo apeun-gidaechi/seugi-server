aaedcab63c50365f1a783123a1bfecfb856ea265
이슬아 <rheeseula@gmail.com>
2024-05-06 16:55:10 +0900

    Feat :: Member 도메인 SaveProfilePort 생성

--- src/main/kotlin/com/seugi/api/domain/member/application/port/out/SaveProfilePort.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/out/SaveProfilePort.kt
@@ -1,4 +1,9 @@
 package com.seugi.api.domain.member.application.port.out
 
-class SaveProfilePort {
+import com.seugi.api.domain.member.adapter.out.entity.ProfileEntity
+
+interface SaveProfilePort {
+
+    fun saveProfile(profile: ProfileEntity)
+
 }
