9a8c9d36a426b2b25218a9618b4b3f4c2f0752fc
이슬아 <rheeseula@gmail.com>
2024-05-06 16:55:52 +0900

    Feat :: Member 도메인 ErrorCode PROFILE_NOT_FOUND 추가

--- src/main/kotlin/com/seugi/api/domain/member/application/exception/MemberErrorCode.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/exception/MemberErrorCode.kt
@@ -12,6 +12,8 @@ enum class MemberErrorCode (
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER-001", "멤버를 찾을 수 없네"),
     MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "MEMBER-002", "멤버가 이미 존재한다"),
     MEMBER_NOT_SUFFICIENT(HttpStatus.PARTIAL_CONTENT, "MEMBER-003", "멤버의 정보가 부족하다"),
-    MEMBER_ALREADY_SUFFICIENT(HttpStatus.ALREADY_REPORTED, "MEMBER-004", "가입이 완료된 계정")
+    MEMBER_ALREADY_SUFFICIENT(HttpStatus.ALREADY_REPORTED, "MEMBER-004", "가입이 완료된 계정"),
+
+    PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER-005", "프로필을 찾을 수 없음")
 
 }
