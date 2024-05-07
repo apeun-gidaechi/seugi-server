8d494649a7911adf48a5f3a7b2b8c7fa44ce6854
이슬아 <rheeseula@gmail.com>
2024-05-08 08:19:32 +0900

    Fix :: Member 도메인 RefreshTokenService 인터페이스 반영
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/service/RefreshTokenService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/RefreshTokenService.kt
@@ -1,15 +1,14 @@
 package com.seugi.api.domain.member.application.service
 
-import com.seugi.api.domain.member.adapter.out.repository.TokenRepository
-import com.seugi.api.domain.member.application.model.value.MemberRefreshToken
+import com.seugi.api.domain.member.adapter.out.entity.MemberTokenEntity
+import com.seugi.api.domain.member.adapter.out.repository.MemberTokenRepository
 import com.seugi.api.domain.member.port.`in`.RefreshTokenUseCase
 import com.seugi.api.domain.member.port.out.LoadMemberPort
-import com.seugi.api.domain.member.port.out.SaveMemberPort
-import com.seugi.api.global.auth.jwt.JwtInfo
 import com.seugi.api.global.auth.jwt.JwtUtils
 import com.seugi.api.global.auth.jwt.exception.JwtErrorCode
 import com.seugi.api.global.exception.CustomException
 import com.seugi.api.global.response.BaseResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 
@@ -17,8 +16,21 @@ import org.springframework.stereotype.Service
 class RefreshTokenService (
     private val jwtUtils: JwtUtils,
     private val loadMemberPort: LoadMemberPort,
-    private val tokenRepository: TokenRepository
+    private val memberTokenRepository: MemberTokenRepository
 ) : RefreshTokenUseCase {
+
+    override fun saveToken(accessToken: String, refreshToken: String) {
+        memberTokenRepository.save(
+            MemberTokenEntity(accessToken, refreshToken)
+        )
+    }
+
+    override fun loadToken(accessToken: String): String {
+        val tokenEntity: MemberTokenEntity = memberTokenRepository.findByIdOrNull(accessToken)
+            ?: throw CustomException(JwtErrorCode.JWT_TOKEN_NOT_VALID)
+
+        return tokenEntity.refreshToken
+    }
 
     override fun refreshToken(token: String): BaseResponse<String> {
         val member = loadMemberPort.loadMemberWithEmail(
@@ -27,10 +39,6 @@ class RefreshTokenService (
             )
         )
 
-        if (!tokenRepository.loadToken(token).isPresent) {
-           throw CustomException(JwtErrorCode.JWT_TOKEN_NOT_VALID)
-        }
-
         return BaseResponse (
             HttpStatus.OK.value(),
             true,
@@ -38,7 +46,5 @@ class RefreshTokenService (
             "리프레시 성공 !",
             jwtUtils.refreshToken(member)
         )
-
     }
-
 }
