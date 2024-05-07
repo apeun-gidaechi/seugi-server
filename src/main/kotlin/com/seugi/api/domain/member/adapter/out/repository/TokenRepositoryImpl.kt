4711624225cf17c4aa4994da7e49a05d7b73cf42
이슬아 <rheeseula@gmail.com>
2024-05-07 11:39:27 +0900

    Feat :: Member 도메인 Adapter RedisRepository 반영
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/adapter/out/repository/TokenRepositoryImpl.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/out/repository/TokenRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.seugi.api.domain.member.adapter.out.repository
 
 import com.seugi.api.global.auth.jwt.JwtProperties
 import org.springframework.data.redis.core.StringRedisTemplate
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
 import java.util.*
 import java.util.concurrent.TimeUnit
