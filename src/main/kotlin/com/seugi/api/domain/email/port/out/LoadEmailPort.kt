a5e9893913d52199dcd515b465b33b7f9610ea50
이슬아 <rheeseula@gmail.com>
2024-05-07 11:40:28 +0900

    Fix :: Member 도메인 Load Email, Token Port 반환 변경
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/email/port/out/LoadEmailPort.kt
+++ src/main/kotlin/com/seugi/api/domain/email/port/out/LoadEmailPort.kt
@@ -1,9 +1,7 @@
 package com.seugi.api.domain.email.port.out
 
-import java.util.*
-
 interface LoadEmailPort {
 
-    fun loadEmail(token: String): Optional<String>
+    fun loadEmail(token: String): String
 
 }
--- src/main/kotlin/com/seugi/api/domain/email/port/out/LoadTokenPort.kt
+++ src/main/kotlin/com/seugi/api/domain/email/port/out/LoadTokenPort.kt
@@ -1,9 +1,10 @@
 package com.seugi.api.domain.email.port.out
 
+import com.seugi.api.domain.email.adapter.out.entity.CodeEntity
 import java.util.*
 
 interface LoadTokenPort {
 
-    fun loadToken(code: String): Optional<String>
+    fun loadToken(code: String): String
 
 }
