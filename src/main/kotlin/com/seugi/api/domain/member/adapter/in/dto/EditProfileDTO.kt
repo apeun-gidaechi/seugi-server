c14694c654a5ef721ddd5b657a9ce51ea95d9ebf
이슬아 <rheeseula@gmail.com>
2024-05-10 17:02:45 +0900

    Feat :: Member 도메인 EditProfileDTO 추가

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/dto/EditProfileDTO.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/dto/EditProfileDTO.kt
@@ -1,4 +1,13 @@
 package com.seugi.api.domain.member.adapter.`in`.dto
 
-class EditProfileDTO {
-}
+class EditProfileDTO (
+
+    val status: String = "", // 상태메시지
+    val nick: String = "", // 닉네임
+    val spot: String = "", // 직위
+    val belong: String = "", // 소속
+    val phone: String = "", // 휴대전화번호
+    val wire: String = "", // 유선전화번호
+    val location: String = "", // 근무위치
+
+)
