87e03d0224396dc0f19af535c2be9bf402f354d9
이슬아 <rheeseula@gmail.com>
2024-05-12 17:15:02 +0900

    Feat :: Member 도메인 EditMemberService editProfile 구현

--- src/main/kotlin/com/seugi/api/domain/member/application/service/EditMemberService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/EditMemberService.kt
@@ -1,18 +1,20 @@
 package com.seugi.api.domain.member.application.service
 
+import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
 import com.seugi.api.domain.member.adapter.out.MemberAdapter
-import com.seugi.api.domain.member.application.exception.MemberErrorCode
 import com.seugi.api.domain.member.application.model.value.MemberPicture
-import com.seugi.api.domain.member.port.`in`.EditMemberUseCase
-import com.seugi.api.global.exception.CustomException
+import com.seugi.api.domain.member.application.port.`in`.EditMemberUseCase
+import com.seugi.api.domain.member.application.port.out.LoadProfilePort
+import com.seugi.api.domain.member.application.port.out.SaveProfilePort
 import com.seugi.api.global.response.BaseResponse
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import java.io.File
 
 @Service
 class EditMemberService (
-    private val memberAdapter: MemberAdapter
+    private val memberAdapter: MemberAdapter,
+    private val loadProfilePort: LoadProfilePort,
+    private val saveProfilePort: SaveProfilePort
 ) : EditMemberUseCase {
 
     override fun editPicture(url: String, id: Long): BaseResponse<Unit> {
@@ -29,4 +31,23 @@ class EditMemberService (
         )
     }
 
+    override fun editProfile(dto: EditProfileDTO, workspaceId: String, id: Long): BaseResponse<Unit> {
+        val profile = loadProfilePort.loadProfileWithMemberIdAndWorkspaceId(id, workspaceId)
+
+        profile.status = dto.status
+        profile.nick = dto.nick
+        profile.belong = dto.belong
+        profile.location = dto.location
+        profile.phone = dto.phone
+        profile.spot = dto.spot
+        profile.wire = dto.wire
+
+        saveProfilePort.saveProfile(profile)
+
+        return BaseResponse (
+            status = HttpStatus.OK.value(),
+            success = true,
+            message = "프로필 수정 성공 !!",
+        )
+    }
 }
