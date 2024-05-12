df8c840aab2fed0386ceb264af5d84a09232d93b
이슬아 <rheeseula@gmail.com>
2024-05-12 17:14:18 +0900

    Feat :: Member 도메인 ProfileAdapter loadProfile, saveProfile 구현

--- src/main/kotlin/com/seugi/api/domain/member/adapter/out/ProfileAdapter.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/out/ProfileAdapter.kt
@@ -5,15 +5,16 @@ import com.seugi.api.domain.member.adapter.out.entity.ProfileEntity
 import com.seugi.api.domain.member.adapter.out.repository.MemberRepository
 import com.seugi.api.domain.member.adapter.out.repository.ProfileRepository
 import com.seugi.api.domain.member.application.exception.MemberErrorCode
-import com.seugi.api.domain.member.application.port.out.profile.LoadProfilePort
+import com.seugi.api.domain.member.application.port.out.LoadProfilePort
+import com.seugi.api.domain.member.application.port.out.SaveProfilePort
 import com.seugi.api.global.exception.CustomException
 import org.springframework.stereotype.Component
 
 @Component
-class MemberProfileAdapter (
-    private val memberRepository: MemberRepository
+class ProfileAdapter (
+    private val memberRepository: MemberRepository,
     private val profileRepository: ProfileRepository
-) : LoadProfilePort {
+) : LoadProfilePort, SaveProfilePort {
 
     override fun loadProfileWithMemberIdAndWorkspaceId(memberId: Long, workspaceId: String): ProfileEntity {
         val member: MemberEntity = memberRepository.findById(memberId)
@@ -22,6 +23,12 @@ class MemberProfileAdapter (
             }
 
         return profileRepository.findByMemberIdAndWorkspaceId(member, workspaceId)
+            .orElseThrow {
+                throw CustomException(MemberErrorCode.PROFILE_NOT_FOUND)
+            }
     }
 
+    override fun saveProfile(profile: ProfileEntity) {
+        profileRepository.save(profile)
+    }
 }
