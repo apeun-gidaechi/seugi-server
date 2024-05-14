835f7a461d52c30da880ea044b59f82c0fd03991
이슬아 <rheeseula@gmail.com>
2024-05-14 19:45:50 +0900

    Feat :: Member 도메인 EditProfileController 생성

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/EditProfileController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/EditProfileController.kt
@@ -1,4 +1,27 @@
 package com.seugi.api.domain.member.adapter.`in`.controller
 
-class EditProfileController {
+import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
+import com.seugi.api.domain.member.application.port.`in`.EditProfileUseCase
+import com.seugi.api.global.common.annotation.GetAuthenticatedId
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/member")
+class EditProfileController (
+    private val editProfileUseCase: EditProfileUseCase
+) {
+
+    @PatchMapping("/profile/{workspaceId}")
+    fun editProfile(@RequestBody dto: EditProfileDTO,
+                    @PathVariable workspaceId: String,
+                    @GetAuthenticatedId id: Long
+    ): BaseResponse<Unit> {
+        return editProfileUseCase.editProfile(dto, workspaceId, id)
+    }
+
 }
