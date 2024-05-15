924956c58b3e61ac19845ae63a28f9d4fb9828e3
이슬아 <rheeseula@gmail.com>
2024-05-15 19:46:18 +0900

    Feat :: Member 도메인 RetrieveProfileDTO 생성

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/dto/RetrieveProfileDTO.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/dto/RetrieveProfileDTO.kt
@@ -1,4 +1,14 @@
 package com.seugi.api.domain.member.adapter.`in`.dto
 
-class RetrieveProfileDTO {
-}
+class RetrieveProfileDTO (
+
+    val workspaceId: String, // 워크스페이스 ID
+    val status: String = "", // 상태메시지
+    val nick: String = "", // 닉네임
+    val spot: String = "", // 직위
+    val belong: String = "", // 소속
+    val phone: String = "", // 휴대전화번호
+    val wire: String = "", // 유선전화번호
+    val location: String = "", // 근무위치
+
+)
