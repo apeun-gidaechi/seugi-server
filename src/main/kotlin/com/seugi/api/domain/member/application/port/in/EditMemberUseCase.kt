e494f84daff02bf6c6f6a9fd109812e1e03c9dfc
이슬아 <rheeseula@gmail.com>
2024-05-15 19:48:00 +0900

    Fix :: Member 도메인 EditMemberUseCase editProfile 분리

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/EditMemberUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/EditMemberUseCase.kt
@@ -1,12 +1,9 @@
 package com.seugi.api.domain.member.application.port.`in`
 
-import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
 import com.seugi.api.global.response.BaseResponse
 
 interface EditMemberUseCase {
 
     fun editPicture(url: String, id: Long): BaseResponse<Unit>
-
-    fun editProfile(dto: EditProfileDTO, workspaceId: String, id: Long): BaseResponse<Unit>
 
 }
