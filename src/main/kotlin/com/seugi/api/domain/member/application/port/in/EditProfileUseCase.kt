443d3bb27dc29fcc8e5e058744a3d011b2b71b94
이슬아 <rheeseula@gmail.com>
2024-05-15 19:48:21 +0900

    Feat :: Member 도메인 EditProfileUseCase 생성

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/EditProfileUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/EditProfileUseCase.kt
@@ -1,4 +1,10 @@
 package com.seugi.api.domain.member.application.port.`in`
 
-class EditProfileUseCase {
+import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
+import com.seugi.api.global.response.BaseResponse
+
+interface EditProfileUseCase {
+
+    fun editProfile(dto: EditProfileDTO, workspaceId: String, id: Long): BaseResponse<Unit>
+
 }
