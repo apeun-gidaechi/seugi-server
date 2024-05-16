756902d46959e5e5567138018f6b2e2ce9af3ddf
이슬아 <rheeseula@gmail.com>
2024-05-16 19:50:41 +0900

    Feat :: Member 도메인 EditProfileService 구현
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/service/EditProfileService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/EditProfileService.kt
@@ -1,4 +1,37 @@
 package com.seugi.api.domain.member.application.service
 
-class EditProfileService {
+import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
+import com.seugi.api.domain.member.application.port.`in`.EditProfileUseCase
+import com.seugi.api.domain.member.application.port.out.LoadProfilePort
+import com.seugi.api.domain.member.application.port.out.SaveProfilePort
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+
+@Service
+class EditProfileService (
+    private val loadProfilePort: LoadProfilePort,
+    private val saveProfilePort: SaveProfilePort
+) : EditProfileUseCase {
+
+    override fun editProfile(dto: EditProfileDTO, workspaceId: String, id: Long): BaseResponse<Unit> {
+        val profile = loadProfilePort.loadProfile(id, workspaceId)
+
+        profile.status = dto.status
+        profile.nick = dto.nick
+        profile.belong = dto.belong
+        profile.location = dto.location
+        profile.phone = dto.phone
+        profile.spot = dto.spot
+        profile.wire = dto.wire
+
+        saveProfilePort.saveProfile(profile)
+
+        return BaseResponse (
+            status = HttpStatus.OK.value(),
+            success = true,
+            message = "프로필 수정 성공 !!",
+        )
+
+    }
 }
