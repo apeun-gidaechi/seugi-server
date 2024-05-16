94235ee413e5e9c84949fd087efa34d35a7ec034
이슬아 <rheeseula@gmail.com>
2024-05-16 19:49:51 +0900

    Feat :: Member 도메인 RetrieveProfileUseCase 생성
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/RetrieveProfileUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/RetrieveProfileUseCase.kt
@@ -1,3 +1,10 @@
 package com.seugi.api.domain.member.application.port.`in`
 
-interface RetrieveMemberUseCase {}
+import com.seugi.api.domain.member.adapter.`in`.dto.RetrieveProfileDTO
+import com.seugi.api.global.response.BaseResponse
+
+interface RetrieveProfileUseCase {
+
+    fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileDTO>
+
+}
