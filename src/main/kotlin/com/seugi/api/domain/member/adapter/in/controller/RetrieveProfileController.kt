d1ee315ac9d5e5e9634fa72466488ddfbba4e1bf
이슬아 <rheeseula@gmail.com>
2024-05-16 19:53:57 +0900

    Feat :: Member 도메인 RetrieveProfileController 생성
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/RetrieveProfileController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/RetrieveProfileController.kt
@@ -1,12 +1,21 @@
 package com.seugi.api.domain.member.adapter.`in`.controller
 
+import com.seugi.api.domain.member.adapter.`in`.dto.RetrieveProfileDTO
 import com.seugi.api.domain.member.application.port.`in`.RetrieveProfileUseCase
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class RetrieveMemberController (
+@RequestMapping("/member")
+class RetrieveProfileController (
     private val retrieveMemberUseCase: RetrieveProfileUseCase
-)
-{
+) {
+
+    @GetMapping("/profile/load")
+    fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileDTO> {
+        return retrieveMemberUseCase.retrieveProfile(workspaceId, memberId)
+    }
 
 }
