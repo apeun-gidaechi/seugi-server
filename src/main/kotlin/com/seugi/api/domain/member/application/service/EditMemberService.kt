f769b18f3d7e7fb251e0ac25c2a47e201a08f99b
이슬아 <rheeseula@gmail.com>
2024-05-16 19:48:59 +0900

    Fix :: Member 도메인 EditMemberService editProfile 제거
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/service/EditMemberService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/EditMemberService.kt
@@ -1,11 +1,8 @@
 package com.seugi.api.domain.member.application.service
 
-import com.seugi.api.domain.member.adapter.`in`.dto.EditProfileDTO
 import com.seugi.api.domain.member.adapter.out.MemberAdapter
 import com.seugi.api.domain.member.application.model.value.MemberPicture
 import com.seugi.api.domain.member.application.port.`in`.EditMemberUseCase
-import com.seugi.api.domain.member.application.port.out.LoadProfilePort
-import com.seugi.api.domain.member.application.port.out.SaveProfilePort
 import com.seugi.api.global.response.BaseResponse
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -13,8 +10,6 @@ import org.springframework.stereotype.Service
 @Service
 class EditMemberService (
     private val memberAdapter: MemberAdapter,
-    private val loadProfilePort: LoadProfilePort,
-    private val saveProfilePort: SaveProfilePort
 ) : EditMemberUseCase {
 
     override fun editPicture(url: String, id: Long): BaseResponse<Unit> {
@@ -31,23 +26,4 @@ class EditMemberService (
         )
     }
 
-    override fun editProfile(dto: EditProfileDTO, workspaceId: String, id: Long): BaseResponse<Unit> {
-        val profile = loadProfilePort.loadProfileWithMemberIdAndWorkspaceId(id, workspaceId)
-
-        profile.status = dto.status
-        profile.nick = dto.nick
-        profile.belong = dto.belong
-        profile.location = dto.location
-        profile.phone = dto.phone
-        profile.spot = dto.spot
-        profile.wire = dto.wire
-
-        saveProfilePort.saveProfile(profile)
-
-        return BaseResponse (
-            status = HttpStatus.OK.value(),
-            success = true,
-            message = "프로필 수정 성공 !!",
-        )
-    }
 }
