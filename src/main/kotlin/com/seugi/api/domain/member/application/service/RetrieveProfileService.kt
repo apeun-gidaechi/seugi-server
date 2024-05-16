652fdc094b0c32c98647165a710fd753e3175335
이슬아 <rheeseula@gmail.com>
2024-05-16 19:50:01 +0900

    Feat :: Member 도메인 RetrieveProfileService 구현
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/service/RetrieveProfileService.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/RetrieveProfileService.kt
@@ -1,7 +1,38 @@
 package com.seugi.api.domain.member.application.service
 
+import com.seugi.api.domain.member.adapter.`in`.dto.RetrieveProfileDTO
+import com.seugi.api.domain.member.application.port.`in`.RetrieveProfileUseCase
+import com.seugi.api.domain.member.application.port.out.LoadProfilePort
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 
 @Service
-class RetrieveMemberService {
+class RetrieveProfileService (
+    private val loadProfilePort: LoadProfilePort
+): RetrieveProfileUseCase {
+
+    override fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileDTO> {
+        val entity = loadProfilePort.loadProfile(memberId, workspaceId)
+
+        val profile = RetrieveProfileDTO (
+            workspaceId = entity.workspaceId,
+            status = entity.status,
+            nick = entity.nick,
+            spot = entity.spot,
+            belong = entity.belong,
+            phone = entity.phone,
+            wire = entity.wire,
+            location = entity.location
+        )
+
+        return BaseResponse (
+            status = HttpStatus.OK.value(),
+            success = true,
+            message = "프로필 불러오기 성공",
+            data = profile
+        )
+
+    }
+
 }
