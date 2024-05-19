01398c7fa52caa5c9e40a24da6bf0bd9f845d565
이슬아 <rheeseula@gmail.com>
2024-05-19 10:09:45 +0900

    Fix :: Profile 도메인 RetrieveProfileResponse 데이터 클래스로 만듦 & 컴페니언 toResponse 메서드 추가

--- src/main/kotlin/com/seugi/api/domain/profile/adapter/in/response/RetrieveProfileResponse.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/adapter/in/response/RetrieveProfileResponse.kt
@@ -1,6 +1,8 @@
 package com.seugi.api.domain.profile.adapter.`in`.response
 
-class RetrieveProfileDTO (
+import com.seugi.api.domain.profile.application.model.Profile
+
+data class RetrieveProfileResponse (
 
     val workspaceId: String, // 워크스페이스 ID
     val status: String = "", // 상태메시지
@@ -11,4 +13,21 @@ class RetrieveProfileDTO (
     val wire: String = "", // 유선전화번호
     val location: String = "", // 근무위치
 
-)
+) {
+    companion object {
+
+        fun toResponse(profile: Profile): RetrieveProfileResponse {
+            return RetrieveProfileResponse (
+                workspaceId = profile.workspaceId.value,
+                status = profile.status.value,
+                nick = profile.nick.value,
+                spot = profile.spot.value,
+                belong = profile.belong.value,
+                phone = profile.phone.value,
+                wire = profile.wire.value,
+                location = profile.location.value
+            )
+        }
+
+    }
+}
