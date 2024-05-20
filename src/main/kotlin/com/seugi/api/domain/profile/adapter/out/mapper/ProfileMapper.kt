f7e5e9275e5234207769660dc72782a94d135c1e
이슬아 <rheeseula@gmail.com>
2024-05-20 10:13:46 +0900

    Feat :: Profile 도메인 Mapper 작성

--- src/main/kotlin/com/seugi/api/domain/profile/adapter/out/mapper/ProfileMapper.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/adapter/out/mapper/ProfileMapper.kt
@@ -1,4 +1,45 @@
 package com.seugi.api.domain.profile.adapter.out.mapper
 
-class ProfileMapper {
+import com.seugi.api.domain.member.adapter.out.mapper.MemberMapper
+import com.seugi.api.domain.profile.adapter.out.entity.ProfileEntity
+import com.seugi.api.domain.profile.application.model.Profile
+import com.seugi.api.domain.profile.application.model.value.*
+import com.seugi.api.global.common.Mapper
+import org.springframework.stereotype.Component
+
+@Component
+class ProfileMapper (
+    private val memberMapper: MemberMapper
+) : Mapper<Profile, ProfileEntity> {
+
+    override fun toDomain(entity: ProfileEntity): Profile {
+        return Profile (
+            belong = ProfileBelong(entity.belong),
+            id = ProfileId(entity.id),
+            location = ProfileLocation(entity.location),
+            memberId = memberMapper.toDomain(entity.memberId),
+            nick = ProfileNick(entity.nick),
+            phone = ProfilePhone(entity.phone),
+            spot = ProfileSpot(entity.spot),
+            status = ProfileStatus(entity.status),
+            wire = ProfileWire(entity.wire),
+            workspaceId = ProfileWorkspaceId(entity.workspaceId)
+        )
+    }
+
+    override fun toEntity(domain: Profile): ProfileEntity {
+        return ProfileEntity (
+            belong = domain.belong.value,
+            id = domain.id.value,
+            location = domain.location.value,
+            memberId = memberMapper.toEntity(domain.memberId),
+            nick = domain.location.value,
+            phone = domain.phone.value,
+            spot = domain.spot.value,
+            status = domain.status.value,
+            wire = domain.wire.value,
+            workspaceId = domain.workspaceId.value
+        )
+    }
+
 }
