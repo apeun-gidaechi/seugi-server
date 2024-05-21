c004498477c19fa4c447fabe22abb72fc7324b87
이슬아 <rheeseula@gmail.com>
2024-05-21 10:19:56 +0900

    Fix :: Profile 도메인 Retrieve 프로필 Controller "/profile/retrieve" 로 매핑 변경
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/profile/adapter/in/controller/RetrieveProfileController.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/adapter/in/controller/RetrieveProfileController.kt
@@ -1,20 +1,24 @@
 package com.seugi.api.domain.profile.adapter.`in`.controller
 
-import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileDTO
+import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileResponse
 import com.seugi.api.domain.profile.application.port.`in`.RetrieveProfileUseCase
 import com.seugi.api.global.response.BaseResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/profile")
 class RetrieveProfileController (
     private val retrieveMemberUseCase: RetrieveProfileUseCase
 ) {
 
-    @GetMapping("/profile/load")
-    fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileDTO> {
+    @GetMapping("/retrieve")
+    fun retrieveProfile(
+        @RequestParam workspaceId: String,
+        @RequestParam memberId: Long
+    ): BaseResponse<RetrieveProfileResponse> {
         return retrieveMemberUseCase.retrieveProfile(workspaceId, memberId)
     }
 
