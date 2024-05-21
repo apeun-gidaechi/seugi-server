a4ebc497aa176713a8d649bb6ba8a44fe0acbe61
이슬아 <rheeseula@gmail.com>
2024-05-21 10:24:40 +0900

    Fix :: Profile 도메인 Adapter 모델 사용
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/profile/adapter/out/ProfileAdapter.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/adapter/out/ProfileAdapter.kt
@@ -1,36 +1,42 @@
 package com.seugi.api.domain.profile.adapter.out
 
-import com.seugi.api.domain.member.adapter.out.entity.MemberEntity
-import com.seugi.api.domain.profile.adapter.out.entity.ProfileEntity
 import com.seugi.api.domain.member.adapter.out.repository.MemberRepository
 import com.seugi.api.domain.profile.adapter.out.repository.ProfileRepository
 import com.seugi.api.domain.member.application.exception.MemberErrorCode
+import com.seugi.api.domain.profile.adapter.out.mapper.ProfileMapper
 import com.seugi.api.domain.profile.application.exception.ProfileErrorCode
+import com.seugi.api.domain.profile.application.model.Profile
 import com.seugi.api.domain.profile.application.port.out.LoadProfilePort
 import com.seugi.api.domain.profile.application.port.out.SaveProfilePort
 import com.seugi.api.global.exception.CustomException
 import org.springframework.stereotype.Component
 
 @Component
-class ProfileAdapter (
+class ProfileAdapter(
     private val memberRepository: MemberRepository,
-    private val profileRepository: ProfileRepository
+    private val profileRepository: ProfileRepository,
+    private val profileMapper: ProfileMapper
 ) : LoadProfilePort, SaveProfilePort {
 
-    override fun loadProfile(memberId: Long, workspaceId: String): ProfileEntity {
-        val member: MemberEntity = memberRepository.findById(memberId)
+    override fun loadProfile(memberId: Long, workspaceId: String): Profile {
+        val memberEntity = memberRepository.findById(memberId)
             .orElseThrow {
-                throw CustomException(MemberErrorCode.MEMBER_NOT_FOUND)
+                CustomException(MemberErrorCode.MEMBER_NOT_FOUND)
             }
 
-        return profileRepository.findByMemberIdAndWorkspaceId(member, workspaceId)
+        val profileEntity = profileRepository.findByMemberIdAndWorkspaceId(memberEntity, workspaceId)
             .orElseThrow {
-                throw CustomException(ProfileErrorCode.PROFILE_NOT_FOUND)
+                CustomException(ProfileErrorCode.PROFILE_NOT_FOUND)
             }
+
+        return profileMapper.toDomain(profileEntity)
+
     }
 
-    override fun saveProfile(profile: ProfileEntity) {
-        profileRepository.save(profile)
+    override fun saveProfile(profile: Profile) {
+        profileRepository.save(
+            profileMapper.toEntity(profile)
+        )
     }
 
 }
