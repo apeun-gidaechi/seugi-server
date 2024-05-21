b4d907a54277255f04a4c9e82688bdeb7061833d
이슬아 <rheeseula@gmail.com>
2024-05-21 10:23:53 +0900

    Fix :: Member 도메인 RetrieveProfileService toResponse 메서드 사용
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/profile/application/service/RetrieveProfileService.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/application/service/RetrieveProfileService.kt
@@ -1,6 +1,6 @@
 package com.seugi.api.domain.profile.application.service
 
-import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileDTO
+import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileResponse
 import com.seugi.api.domain.profile.application.port.`in`.RetrieveProfileUseCase
 import com.seugi.api.domain.profile.application.port.out.LoadProfilePort
 import com.seugi.api.global.response.BaseResponse
@@ -12,25 +12,14 @@ class RetrieveProfileService (
     private val loadProfilePort: LoadProfilePort
 ): RetrieveProfileUseCase {
 
-    override fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileDTO> {
-        val entity = loadProfilePort.loadProfile(memberId, workspaceId)
-
-        val profile = RetrieveProfileDTO (
-            workspaceId = entity.workspaceId,
-            status = entity.status,
-            nick = entity.nick,
-            spot = entity.spot,
-            belong = entity.belong,
-            phone = entity.phone,
-            wire = entity.wire,
-            location = entity.location
-        )
+    override fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileResponse> {
+        val profile = loadProfilePort.loadProfile(memberId, workspaceId)
 
         return BaseResponse (
             status = HttpStatus.OK.value(),
             success = true,
             message = "프로필 불러오기 성공",
-            data = profile
+            data = RetrieveProfileResponse.toResponse(profile)
         )
 
     }
