edefdcd69e179ad22deb3f5ac27d59668d41c43b
이슬아 <rheeseula@gmail.com>
2024-05-21 10:21:57 +0900

    Fix :: Profile 도메인 RetrieveProfileUseCase BaseResponse 제네릭 타입 변경
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/profile/application/port/in/RetrieveProfileUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/application/port/in/RetrieveProfileUseCase.kt
@@ -1,10 +1,10 @@
 package com.seugi.api.domain.profile.application.port.`in`
 
-import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileDTO
+import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileResponse
 import com.seugi.api.global.response.BaseResponse
 
 interface RetrieveProfileUseCase {
 
-    fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileDTO>
+    fun retrieveProfile(workspaceId: String, memberId: Long): BaseResponse<RetrieveProfileResponse>
 
 }
