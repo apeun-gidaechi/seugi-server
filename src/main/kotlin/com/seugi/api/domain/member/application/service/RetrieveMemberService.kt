82dfda23c724f98104068f49891d072ef3553db2
이슬아 <rheeseula@gmail.com>
2024-05-22 21:16:45 +0900

    Feat :: Member 도메인 RetrieveMemberService 구현
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

---
+++ src/main/kotlin/com/seugi/api/domain/member/application/service/RetrieveMemberService.kt
@@ -0,0 +1,26 @@
+package com.seugi.api.domain.member.application.service
+
+import com.seugi.api.domain.member.adapter.`in`.dto.res.RetrieveMemberResponse
+import com.seugi.api.domain.member.application.model.Member
+import com.seugi.api.domain.member.application.port.`in`.RetrieveMemberUseCase
+import com.seugi.api.domain.member.application.port.out.LoadMemberPort
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+
+@Service
+class RetrieveMemberService (
+    private val loadMemberPort: LoadMemberPort
+) : RetrieveMemberUseCase {
+
+    override fun retrieveMember(memberId: Long): BaseResponse<RetrieveMemberResponse> {
+        val member: Member = loadMemberPort.loadMemberWithId(memberId)
+
+        return BaseResponse (
+            status = HttpStatus.OK.value(),
+            success = true,
+            message = "멤버 정보 조회 성공 !!",
+            data = RetrieveMemberResponse.toResponse(member)
+        )
+    }
+}
