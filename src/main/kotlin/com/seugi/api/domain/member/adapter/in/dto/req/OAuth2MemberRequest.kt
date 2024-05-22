0369ad985c4cec37a46541c5da7a50f91821ecaa
이슬아 <rheeseula@gmail.com>
2024-05-22 19:30:52 +0900

    Fix :: Member 도메인 OAuth2MemberDTO Request로 변경
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/dto/req/OAuth2MemberRequest.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/dto/req/OAuth2MemberRequest.kt
@@ -1,8 +1,8 @@
-package com.seugi.api.domain.member.adapter.`in`.dto
+package com.seugi.api.domain.member.adapter.`in`.dto.req
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-class OAuth2MemberDTO (
+class OAuth2MemberRequest (
 
     @JsonProperty("email") val email: String,
     @JsonProperty("name") val name: String,
