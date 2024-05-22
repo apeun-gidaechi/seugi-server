55e86da453ab81e33c1ce777425fbc6b55f4ea36
이슬아 <rheeseula@gmail.com>
2024-05-22 19:32:38 +0900

    Fix :: Member 도메인 RegisterMemberDTO 개명 반영
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/RegisterMemberUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/RegisterMemberUseCase.kt
@@ -1,10 +1,10 @@
 package com.seugi.api.domain.member.application.port.`in`
 
-import com.seugi.api.domain.member.adapter.`in`.dto.RegisterMemberDTO
+import com.seugi.api.domain.member.adapter.`in`.dto.req.RegisterMemberRequest
 import com.seugi.api.global.response.BaseResponse
 
 interface RegisterMemberUseCase {
 
-    fun registerMember(memberDTO: RegisterMemberDTO): BaseResponse<String>
+    fun registerMember(memberDTO: RegisterMemberRequest): BaseResponse<String>
 
 }
