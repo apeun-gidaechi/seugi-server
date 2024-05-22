4f1deeacd7238a39eac1f9742147112e12696751
이슬아 <rheeseula@gmail.com>
2024-05-22 19:32:26 +0900

    Fix :: Member 도메인 OAuth2MemberDTO 개명 반영
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/OAuth2MemberUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/OAuth2MemberUseCase.kt
@@ -1,7 +1,7 @@
 package com.seugi.api.domain.member.application.port.`in`
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.seugi.api.domain.member.adapter.`in`.dto.OAuth2MemberDTO
+import com.seugi.api.domain.member.adapter.`in`.dto.req.OAuth2MemberRequest
 import com.seugi.api.global.auth.jwt.JwtInfo
 import com.seugi.api.global.response.BaseResponse
 
@@ -9,7 +9,7 @@ interface OAuth2MemberUseCase {
 
     fun process(code: String, provider: String): BaseResponse<JwtInfo>
 
-    fun complete(dto: OAuth2MemberDTO): BaseResponse<Unit>
+    fun complete(dto: OAuth2MemberRequest): BaseResponse<Unit>
 
     fun getAccessToken(code: String): String
 
