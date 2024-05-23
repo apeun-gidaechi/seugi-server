2d6c0cfc33c6277c8fa17a3ddd1cfbfdef14b77d
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-05-23 19:19:46 +0900

    Refactor :: 리팩토링시 필요없는 파일 삭제 or 주석처리 #133

--- src/main/kotlin/com/seugi/api/domain/chat/domain/joined/mapper/JoinedMapper.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/domain/joined/mapper/JoinedMapper.kt
@@ -1,46 +1,45 @@
-package com.seugi.api.domain.chat.domain.joined.mapper
-
-import com.seugi.api.domain.chat.domain.enums.type.RoomType
-import com.seugi.api.domain.chat.domain.joined.JoinedEntity
-import com.seugi.api.domain.chat.domain.joined.model.Joined
-import com.seugi.api.global.common.Mapper
-import org.springframework.stereotype.Component
-
-@Component
-class JoinedMapper : Mapper<Joined, JoinedEntity> {
-    override fun toDomain(entity: JoinedEntity): Joined {
-        return Joined(
-            joinUserId = entity.joinedUserId,
-            roomType = entity.roomType,
-            roomAdmin = entity.roomAdmin,
-            chatRoomId = entity.chatRoomId!!,
-            workspaceId = entity.workspaceID
-        )
-    }
-
-    override fun toEntity(domain: Joined): JoinedEntity {
-        return JoinedEntity(
-            joinedUserId = domain.joinUserId,
-            roomType = domain.roomType,
-            roomAdmin = domain.roomAdmin,
-            chatRoomId = domain.chatRoomId,
-            workspaceID = domain.workspaceId
-        )
-    }
-
-    fun toEntity(
-        chatRoomId: Long,
-        joinedUserId: List<Long>,
-        type: RoomType,
-        roomAdmin: Long,
-        workspaceId: String
-    ): JoinedEntity {
-        return JoinedEntity(
-            joinedUserId = joinedUserId.toMutableSet(),
-            roomType = type,
-            roomAdmin = roomAdmin,
-            chatRoomId = chatRoomId,
-            workspaceID = workspaceId
-        )
-    }
-}
+//package com.seugi.api.domain.chat.domain.joined.mapper
+//
+//import com.seugi.api.domain.chat.domain.enums.type.RoomType
+//import com.seugi.api.domain.chat.domain.joined.model.Joined
+//import com.seugi.api.global.common.Mapper
+//import org.springframework.stereotype.Component
+//
+//@Component
+//class JoinedMapper : Mapper<Joined, JoinedEntity> {
+//    override fun toDomain(entity: JoinedEntity): Joined {
+//        return Joined(
+//            joinUserId = entity.joinedUserId,
+//            roomType = entity.roomType,
+//            roomAdmin = entity.roomAdmin,
+//            chatRoomId = entity.chatRoomId!!,
+//            workspaceId = entity.workspaceID
+//        )
+//    }
+//
+//    override fun toEntity(domain: Joined): JoinedEntity {
+//        return JoinedEntity(
+//            joinedUserId = domain.joinUserId,
+//            roomType = domain.roomType,
+//            roomAdmin = domain.roomAdmin,
+//            chatRoomId = domain.chatRoomId,
+//            workspaceID = domain.workspaceId
+//        )
+//    }
+//
+//    fun toEntity(
+//        chatRoomId: Long,
+//        joinedUserId: List<Long>,
+//        type: RoomType,
+//        roomAdmin: Long,
+//        workspaceId: String
+//    ): JoinedEntity {
+//        return JoinedEntity(
+//            joinedUserId = joinedUserId.toMutableSet(),
+//            roomType = type,
+//            roomAdmin = roomAdmin,
+//            chatRoomId = chatRoomId,
+//            workspaceID = workspaceId
+//        )
+//    }
+//}
