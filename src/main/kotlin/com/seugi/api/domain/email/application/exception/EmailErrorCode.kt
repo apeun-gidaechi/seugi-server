25c068a3887e7617f3996cb2f997e4b639c50df9
이슬아 <rheeseula@gmail.com>
2024-05-25 21:25:59 +0900

    Feat :: Email 도메인 ErrorCode CODE_NOT_MATCH 추가

--- src/main/kotlin/com/seugi/api/domain/email/application/exception/EmailErrorCode.kt
+++ src/main/kotlin/com/seugi/api/domain/email/application/exception/EmailErrorCode.kt
@@ -11,8 +11,7 @@ enum class EmailErrorCode (
 
 ) : CustomErrorCode {
 
-    CODE_NOT_EXIST(HttpStatus.NOT_FOUND, "EMAIL-001", "코드가 존재하지 않아요"),
-    EMAIL_NOT_LOADED(HttpStatus.NOT_FOUND, "EMAIL-002", "이메일을 불러오지 못했어요"),
-    EMAIL_NOT_MATCH(HttpStatus.BAD_REQUEST, "EMAIL-003", "이메일이 일치하지 않아요")
+    EMAIL_NOT_LOADED(HttpStatus.NOT_FOUND, "EMAIL-001", "이메일을 불러오지 못했어요"),
+    CODE_NOT_MATCH(HttpStatus.CONFLICT, "EMAIL-002", "코드가 일치하지 않아요"),
 
 }
