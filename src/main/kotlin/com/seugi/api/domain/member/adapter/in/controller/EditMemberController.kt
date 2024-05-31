57b2110c008bfb88d7909154611f62f9449533ae
이슬아 <rheeseula@gmail.com>
2024-05-31 09:48:21 +0900

    Fix :: Member 도메인 Edit Controller editMember url 파람 dto로 변경

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/EditMemberController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/EditMemberController.kt
@@ -1,21 +1,23 @@
 package com.seugi.api.domain.member.adapter.`in`.controller
 
+import com.seugi.api.domain.member.adapter.`in`.dto.req.EditMemberRequest
 import com.seugi.api.domain.member.application.port.`in`.EditMemberUseCase
 import com.seugi.api.global.common.annotation.GetAuthenticatedId
 import com.seugi.api.global.response.BaseResponse
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/member/edit")
+@RequestMapping("/member")
 class EditMemberController (
     private val editMemberUseCase: EditMemberUseCase
 ) {
 
-    @GetMapping("/picture")
-    fun editPicture(url: String, @GetAuthenticatedId id: Long) : BaseResponse<Unit> {
-        return editMemberUseCase.editPicture(url, id)
+    @PatchMapping("/edit")
+    fun editMember(@RequestBody dto: EditMemberRequest, @GetAuthenticatedId id: Long) : BaseResponse<Unit> {
+        return editMemberUseCase.editMember(dto, id)
     }
 
 }
