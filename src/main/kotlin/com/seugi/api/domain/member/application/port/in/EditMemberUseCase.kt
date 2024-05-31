267f658a7655419fba2ca5637d54823130dfc3a9
이슬아 <rheeseula@gmail.com>
2024-05-31 09:47:18 +0900

    Fix :: Member 도메인 EditMemberUseCase editMember url 파람 dto로 변경

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/EditMemberUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/EditMemberUseCase.kt
@@ -1,9 +1,10 @@
 package com.seugi.api.domain.member.application.port.`in`
 
+import com.seugi.api.domain.member.adapter.`in`.dto.req.EditMemberRequest
 import com.seugi.api.global.response.BaseResponse
 
 interface EditMemberUseCase {
 
-    fun editPicture(url: String, id: Long): BaseResponse<Unit>
+    fun editMember(dto: EditMemberRequest, id: Long): BaseResponse<Unit>
 
 }
