37ebef073c10fcbc68400fad77f862e74d6ab073
이슬아 <rheeseula@gmail.com>
2024-06-01 09:37:54 +0900

    Feat :: Profile 도메인 CreateProfileUseCase 구현

--- src/main/kotlin/com/seugi/api/domain/profile/application/service/CreateProfileService.kt
+++ src/main/kotlin/com/seugi/api/domain/profile/application/service/CreateProfileService.kt
@@ -1,4 +1,27 @@
 package com.seugi.api.domain.profile.application.service
 
-class CreateProfileService {
+import com.seugi.api.domain.member.application.port.out.LoadMemberPort
+import com.seugi.api.domain.profile.application.model.Profile
+import com.seugi.api.domain.profile.application.port.`in`.CreateProfileUseCase
+import com.seugi.api.domain.profile.application.port.out.ExistProfilePort
+import com.seugi.api.domain.profile.application.port.out.SaveProfilePort
+import org.springframework.stereotype.Service
+
+@Service
+class CreateProfileService (
+    private val existProfilePort: ExistProfilePort,
+    private val loadMemberPort: LoadMemberPort,
+    private val saveProfilePort: SaveProfilePort
+) : CreateProfileUseCase {
+
+    override fun createProfile(memberId: Long, workspaceId: String) {
+        if (!existProfilePort.existProfile(memberId, workspaceId)) {
+            val member = loadMemberPort.loadMemberWithId(memberId)
+
+            val profile = Profile(member, workspaceId)
+
+            saveProfilePort.saveProfile(profile)
+        }
+    }
+
 }
