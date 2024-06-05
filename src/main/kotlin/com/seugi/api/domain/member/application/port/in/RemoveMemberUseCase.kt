5c96198c6638cce3783230ef220b6de6b4c0c232
이슬아 <rheeseula@gmail.com>
2024-06-05 17:53:47 +0900

    Feat :: Member 도메인 RemoveMemberUseCase 추가

--- src/main/kotlin/com/seugi/api/domain/member/application/port/in/RemoveMemberUseCase.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/port/in/RemoveMemberUseCase.kt
@@ -1,4 +1,9 @@
 package com.seugi.api.domain.member.application.port.`in`
 
+import com.seugi.api.global.response.BaseResponse
+
 interface RemoveMemberUseCase {
+
+    fun removeMember(id: Long) : BaseResponse<Unit>
+
 }
