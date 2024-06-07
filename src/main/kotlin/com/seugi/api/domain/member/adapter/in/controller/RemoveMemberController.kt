7eae644919a7a78dcaf3f28d16842a82f0894475
이슬아 <rheeseula@gmail.com>
2024-06-07 17:57:07 +0900

    Feat :: Member 도메인 RemoveMemberController 추가

--- src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/RemoveMemberController.kt
+++ src/main/kotlin/com/seugi/api/domain/member/adapter/in/controller/RemoveMemberController.kt
@@ -1,4 +1,21 @@
 package com.seugi.api.domain.member.adapter.`in`.controller
 
-class RemoveMemberController {
+import com.seugi.api.domain.member.application.port.`in`.RemoveMemberUseCase
+import com.seugi.api.global.common.annotation.GetAuthenticatedId
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/member")
+class RemoveMemberController(
+    private val removeMemberUseCase: RemoveMemberUseCase
+) {
+
+    @GetMapping("/remove")
+    fun removeMember(@GetAuthenticatedId id: Long): BaseResponse<Unit> {
+        return removeMemberUseCase.removeMember(id)
+    }
+
 }
