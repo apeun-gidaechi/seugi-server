76898129161ac9708caf02c5cea228ac13ef891f
이슬아 <rheeseula@gmail.com>
2024-06-15 17:16:26 +0900

    Feat :: Workspace 도메인 WorkspacememberListResponse 추가

--- src/main/kotlin/com/seugi/api/domain/workspace/presentation/dto/response/WorkspaceMemberListResponse.kt
+++ src/main/kotlin/com/seugi/api/domain/workspace/presentation/dto/response/WorkspaceMemberListResponse.kt
@@ -1,4 +1,8 @@
 package com.seugi.api.domain.workspace.presentation.dto.response
 
-class WorkspaceMemberListResponse {
-}
+import com.seugi.api.domain.profile.application.model.Profile
+
+class WorkspaceMemberListResponse (
+    val teachers: MutableMap<String, List<Profile>> = HashMap(),
+    val students: MutableMap<String, List<Profile>> = HashMap()
+)
