87d8e997c6caa0e0586f407316801dfe7c27976f
이슬아 <rheeseula@gmail.com>
2024-06-20 08:42:14 +0900

    Fix :: Workspace 도메인 MemberListResponse MemberChartResponse로 변경
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/workspace/presentation/dto/response/WorkspaceMemberChartResponse.kt
+++ src/main/kotlin/com/seugi/api/domain/workspace/presentation/dto/response/WorkspaceMemberChartResponse.kt
@@ -1,9 +1,8 @@
 package com.seugi.api.domain.workspace.presentation.dto.response
 
 import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileResponse
-import com.seugi.api.domain.profile.application.model.Profile
 
-class WorkspaceMemberListResponse (
+class WorkspaceMemberChartResponse (
     val teachers: MutableMap<String, List<RetrieveProfileResponse>> = HashMap(),
     val students: MutableMap<String, List<RetrieveProfileResponse>> = HashMap()
 )
