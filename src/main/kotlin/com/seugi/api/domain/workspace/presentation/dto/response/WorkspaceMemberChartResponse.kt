a2ae91e6e2e2a153342df2ca1e6a9df68422e959
이슬아 <rheeseula@gmail.com>
2024-06-25 20:25:47 +0900

    Feat :: Workspace 도메인 Chart 응답 admin, middleAdmin 추가
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/domain/workspace/presentation/dto/response/WorkspaceMemberChartResponse.kt
+++ src/main/kotlin/com/seugi/api/domain/workspace/presentation/dto/response/WorkspaceMemberChartResponse.kt
@@ -3,6 +3,8 @@ package com.seugi.api.domain.workspace.presentation.dto.response
 import com.seugi.api.domain.profile.adapter.`in`.response.RetrieveProfileResponse
 
 class WorkspaceMemberChartResponse (
+    val admin: MutableMap<String, List<RetrieveProfileResponse>> = HashMap(),
+    val middleAdmin: MutableMap<String, List<RetrieveProfileResponse>> = HashMap(),
     val teachers: MutableMap<String, List<RetrieveProfileResponse>> = HashMap(),
     val students: MutableMap<String, List<RetrieveProfileResponse>> = HashMap()
 )
