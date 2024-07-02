d44851ab0ade5dd0b7260428ae84197edf42cfbf
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-02 14:58:07 +0900

    Fix :: RoomResponse add JsonInclude
    클라이언트 반환시 lastMessageTimestamp 가 없을때 null 빈 문자열이 반환되던걸 null 보내달라는 클라이언트의 요구사항을 반영하여 jsonInclude를 사용하여 null 값 반환하도록 수정

--- src/main/kotlin/com/seugi/api/domain/chat/presentation/chat/room/dto/response/RoomResponse.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/presentation/chat/room/dto/response/RoomResponse.kt
@@ -1,9 +1,11 @@
 package com.seugi.api.domain.chat.presentation.chat.room.dto.response
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.seugi.api.domain.chat.domain.enums.status.ChatStatusEnum
 import com.seugi.api.domain.chat.domain.enums.type.RoomType
 import com.seugi.api.domain.member.adapter.`in`.dto.res.RetrieveMemberResponse
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class RoomResponse(
     val id: String,
     val workspaceId: String,
