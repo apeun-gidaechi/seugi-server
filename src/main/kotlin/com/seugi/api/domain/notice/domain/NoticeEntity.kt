f4b290922684aaa2a003270d483161a5421f4618
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-05 19:18:12 +0900

    Feat :: Notice에 대한 모델과 엔티티 추가
    Notice 모델과 Entity를 추가하였습니다.

--- src/main/kotlin/com/seugi/api/domain/notice/domain/NoticeEntity.kt
+++ src/main/kotlin/com/seugi/api/domain/notice/domain/NoticeEntity.kt
@@ -1,16 +1,45 @@
 package com.seugi.api.domain.notice.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import com.seugi.api.domain.member.adapter.out.entity.MemberEntity
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 class NoticeEntity(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+    val id: Long? = null,
 
+    @Column(nullable = false)
+    val workspaceId: String,
 
-    )
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    var user: MemberEntity? = null,
+
+    @Column(nullable = false)
+    var title: String,
+
+    @Column(nullable = false)
+    var content: String,
+
+    @ElementCollection
+    @Column(nullable = false)
+    val emoji: List<String> = emptyList(),
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    var creationDate: LocalDateTime? = null,
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    var lastModifiedDate: LocalDateTime? = null,
+
+    @Column(nullable = false)
+    var deleted: Boolean = false,
+)
