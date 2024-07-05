62f53ffb44e356ca56bf96b407231384bc4478fb
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-05 19:18:06 +0900

    Feat :: Add NoticeRepository And RepositoryCustom
    NoticeRepository 를 만들고 QueryDSL 설정을 위하여 Custom 레포도 만들었습니다 또한 공지 조회를 위한 findByWorkspaceId 조회 로직을 추가하였습니다.

--- src/main/kotlin/com/seugi/api/domain/notice/domain/NoticeRepository.kt
+++ src/main/kotlin/com/seugi/api/domain/notice/domain/NoticeRepository.kt
@@ -2,4 +2,6 @@ package com.seugi.api.domain.notice.domain
 
 import org.springframework.data.repository.CrudRepository
 
-interface NoticeRepository : CrudRepository<NoticeEntity, Long>
+interface NoticeRepository : CrudRepository<NoticeEntity, Long>, NoticeRepositoryCustom {
+    override fun findByWorkspaceId(workspaceId: String): List<NoticeEntity>
+}
