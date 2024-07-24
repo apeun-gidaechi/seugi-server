4a2fef5ded851ef8402860930e17f8e3f92306b6
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-24 11:48:21 +0900

    Feat :: 유저가 워크스페이스에 참여시 오류 발생 로직 추가

--- src/main/kotlin/com/seugi/api/domain/workspace/service/WorkspaceServiceImpl.kt
+++ src/main/kotlin/com/seugi/api/domain/workspace/service/WorkspaceServiceImpl.kt
@@ -155,16 +155,29 @@ class WorkspaceServiceImpl(
 
     }
 
+    private fun checkExistInWorkspace(userId: Long, workspace: WorkspaceEntity) {
+        if (workspace.workspaceAdmin == userId || workspace.middleAdmin.contains(userId) || workspace.teacher.contains(
+                userId
+            ) || workspace.student.contains(userId)
+        ) throw CustomException(WorkspaceErrorCode.EXIST)
+    }
+
     @Transactional
     override fun joinWorkspace(userId: Long, joinWorkspaceRequest: JoinWorkspaceRequest): BaseResponse<Unit> {
 
         val workspace: WorkspaceEntity = findWorkspaceById(joinWorkspaceRequest.workspaceId)
         if (workspace.workspaceCode != joinWorkspaceRequest.workspaceCode) throw CustomException(WorkspaceErrorCode.NOT_MATCH)
 
-
         when (joinWorkspaceRequest.role) {
-            WorkspaceRole.STUDENT -> workspace.studentWaitList.add(userId)
-            WorkspaceRole.TEACHER -> workspace.teacherWaitList.add(userId)
+            WorkspaceRole.STUDENT -> {
+                checkExistInWorkspace(userId, workspace)
+                workspace.studentWaitList.add(userId)
+            }
+
+            WorkspaceRole.TEACHER -> {
+                checkExistInWorkspace(userId, workspace)
+                workspace.teacherWaitList.add(userId)
+            }
             WorkspaceRole.MIDDLE_ADMIN -> Unit
         }
 
