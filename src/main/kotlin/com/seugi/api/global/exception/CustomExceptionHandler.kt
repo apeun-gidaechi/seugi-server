d7de6f0de46a77eccd0a4a3cffac2a472c51a84e
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-07-30 22:14:53 +0900

    Fix :: 디스코드 배포 환경 알림 및 스택 트레이스에 로그 출력하도록 변경
    기존 알림이 전체로 갔다면 배포환경에서만 작동하도록 수정하였습니다 또한 핸들링시 e.printStackTrace() 안하면 기존 출력되던 로그가 안찍혀 그 부분을 추가했으며 메서드 분리까지 수행하였습니다.

--- src/main/kotlin/com/seugi/api/global/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/seugi/api/global/exception/CustomExceptionHandler.kt
@@ -1,12 +1,19 @@
 package com.seugi.api.global.exception
 
+import com.seugi.api.global.infra.discord.webhook.DiscordErrorSendService
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import com.seugi.api.global.response.BaseResponse
+import org.springframework.web.context.request.WebRequest
+
 
 @RestControllerAdvice
-class CustomExceptionHandler {
+class CustomExceptionHandler(
+    private val discordService: DiscordErrorSendService,
+    private val environment: Environment,
+) {
 
     @ExceptionHandler(CustomException::class)
     fun handleCustomException(customException: CustomException): ResponseEntity<Any> {
@@ -18,6 +25,18 @@ class CustomExceptionHandler {
         )
 
         return ResponseEntity(response, customException.customErrorCode.status)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: Exception, webRequest: WebRequest) {
+        e.printStackTrace()
+        sendDiscordAlert(e, webRequest)
+    }
+
+    fun sendDiscordAlert(e: Exception, webRequest: WebRequest) {
+        if (environment.activeProfiles.contains("prod")) {
+            discordService.sendDiscordAlarm(e, webRequest)
+        }
     }
 
 }
