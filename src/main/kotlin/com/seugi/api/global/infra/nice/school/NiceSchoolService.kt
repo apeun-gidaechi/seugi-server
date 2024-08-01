841697e063a18899282d30a0e773b18340891dd7
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-01 23:49:11 +0900

    Feat :: Nice 학교정보 조회 로직 추가 #188
    학교정보를 불러오는 로직을 추가하였습니다.

---
+++ src/main/kotlin/com/seugi/api/global/infra/nice/school/NiceSchoolService.kt
@@ -0,0 +1,45 @@
+package com.seugi.api.global.infra.nice.school
+
+import com.seugi.api.domain.workspace.domain.model.SchoolInfo
+import com.seugi.api.global.infra.nice.school.info.SchoolInfoClient
+import com.seugi.api.global.infra.nice.school.info.SchoolInfoResponse
+import com.seugi.api.global.infra.nice.school.info.SchoolRow
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class NiceSchoolService(
+    private val schoolInfoClient: SchoolInfoClient,
+    @Value("\${nice.key}") private val key: String,
+) {
+
+    fun getSchoolInfo(schoolName: String): SchoolInfo {
+        val schoolResponse: SchoolInfoResponse = schoolInfoClient.getSchoolInfo(
+            key = key,
+            type = "json",
+            pIndex = 1,
+            pSize = 5,
+            schoolNm = schoolName
+        )
+
+        val schoolData: SchoolRow? = schoolResponse.schoolInfos?.get(1)?.row?.get(0)
+
+        return if (schoolData == null) {
+            SchoolInfo(
+                scCode = "",
+                sdCode = "",
+                scType = ""
+            )
+        } else {
+            SchoolInfo(
+                scCode = schoolData.atptOfcdcScCode!!,
+                sdCode = schoolData.sdSchulCode!!,
+                scType = schoolData.schulKndScNm!!
+            )
+        }
+
+
+    }
+
+
+}
