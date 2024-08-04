e7e17a53b38cd304982a919088dfd65f85af2434
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-05 01:39:40 +0900

    Feat :: 급식 Repository 생성 및 QueryDSL 을 위한 커스텀 세팅 #188

---
+++ src/main/kotlin/com/seugi/api/domain/meal/domain/MealRepositoryCustom.kt
@@ -0,0 +1,3 @@
+package com.seugi.api.domain.meal.domain
+
+interface MealRepositoryCustom
---
+++ src/main/kotlin/com/seugi/api/domain/meal/domain/MealRepositoryCustomImpl.kt
@@ -0,0 +1,3 @@
+package com.seugi.api.domain.meal.domain
+
+class MealRepositoryCustomImpl : MealRepositoryCustom
