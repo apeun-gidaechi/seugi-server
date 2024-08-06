72791ea45c0c7e6634a53cffced4fd1202b8b3d1
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-06 21:32:23 +0900

    Feat :: 앞 커밋을 기반한 급식 컨트롤러 생성 #188
    급식 날짜별 조회, 급식 전체 조회 기능 및 급식 다시 조회해 저장하기 로직 API 설계 및 구성

---
+++ src/main/kotlin/com/seugi/api/domain/meal/presentation/MealController.kt
@@ -0,0 +1,43 @@
+package com.seugi.api.domain.meal.presentation
+
+import com.seugi.api.domain.meal.domain.model.Meal
+import com.seugi.api.domain.meal.service.MealService
+import com.seugi.api.global.response.BaseResponse
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/meal")
+class MealController(
+    private val mealService: MealService,
+) {
+
+    @PostMapping("/reset/{workspaceId}")
+    fun resetMeal(
+        @PathVariable workspaceId: String,
+    ): BaseResponse<Unit> {
+        return mealService.resetMealByWorkspaceId(
+            workspaceId = workspaceId
+        )
+    }
+
+    @GetMapping
+    fun getMealByDate(
+        @RequestParam("workspaceId") workspaceId: String,
+        @RequestParam("date") date: String,
+    ): BaseResponse<List<Meal>> {
+        return mealService.getMealByDate(
+            workspaceId = workspaceId,
+            mealDate = date
+        )
+    }
+
+    @GetMapping("/all")
+    fun getAllMeals(
+        @RequestParam("workspaceId") workspaceId: String,
+    ): BaseResponse<List<Meal>> {
+        return mealService.getAllMeals(
+            workspaceId = workspaceId
+        )
+    }
+
+}
