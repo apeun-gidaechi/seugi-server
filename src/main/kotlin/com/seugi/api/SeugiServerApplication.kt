19263a41f1f78516a9f7085d19ad5fc55ffa0acc
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-07 19:31:20 +0900

    Feat :: Add @EnableScheduling in Application #188

--- src/main/kotlin/com/seugi/api/SeugiServerApplication.kt
+++ src/main/kotlin/com/seugi/api/SeugiServerApplication.kt
@@ -3,9 +3,11 @@ package com.seugi.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 class SeugiServerApplication
 
 fun main(args: Array<String>) {
