c3e93adb9cd3f2a96237b2df7537de7aac8df062
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-14 22:12:12 +0900

    Feat :: 학교 시간표 응답 모델 추가
    학교 시간표 정보를 처리하기 위해 새로운 데이터 클래스를 추가했습니다. 이 클래스들은 각 학년별 시간표 데이터를 포함합니다.

---
+++ src/main/kotlin/com/seugi/api/global/infra/nice/school/timetable/SchoolTimetableResponse.kt
@@ -0,0 +1,49 @@
+package com.seugi.api.global.infra.nice.school.timetable
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class HisTimetable(
+    @JsonProperty("hisTimetable") val hisTimetable: List<Item>,
+)
+
+data class MisTimetable(
+    @JsonProperty("misTimetable") val misTimetable: List<Item>,
+)
+
+data class ElsTimetable(
+    @JsonProperty("elsTimetable") val elsTimetable: List<Item>,
+)
+
+data class Item(
+    @JsonProperty("head") val head: List<Head>? = null,
+    @JsonProperty("row") val row: List<Row>? = null,
+)
+
+data class Head(
+    @JsonProperty("list_total_count") val listTotalCount: Int? = null,
+    @JsonProperty("RESULT") val result: Result? = null,
+)
+
+data class Result(
+    @JsonProperty("CODE") val code: String,
+    @JsonProperty("MESSAGE") val message: String,
+)
+
+data class Row(
+    @JsonProperty("ATPT_OFCDC_SC_CODE") val atptOfcdcScCode: String,
+    @JsonProperty("ATPT_OFCDC_SC_NM") val atptOfcdcScNm: String,
+    @JsonProperty("SD_SCHUL_CODE") val sdSchulCode: String,
+    @JsonProperty("SCHUL_NM") val schulNm: String,
+    @JsonProperty("AY") val ay: String,
+    @JsonProperty("SEM") val sem: String,
+    @JsonProperty("ALL_TI_YMD") val allTiYmd: String,
+    @JsonProperty("DGHT_CRSE_SC_NM") val dghtCrseScNm: String,
+    @JsonProperty("ORD_SC_NM") val ordScNm: String,
+    @JsonProperty("DDDEP_NM") val dddepNm: String,
+    @JsonProperty("GRADE") val grade: String,
+    @JsonProperty("CLRM_NM") val clrmNm: String,
+    @JsonProperty("CLASS_NM") val classNm: String,
+    @JsonProperty("PERIO") val perio: String,
+    @JsonProperty("ITRT_CNTNT") val itrtCntnt: String,
+    @JsonProperty("LOAD_DTM") val loadDtm: String,
+)
