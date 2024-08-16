4f090b043b25719882a07888062eb5e04f9bd991
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-16 22:23:16 +0900

    Feat :: Timetable 모델 정의

---
+++ src/main/kotlin/com/seugi/api/domain/timetable/domain/model/Timetable.kt
@@ -0,0 +1,9 @@
+package com.seugi.api.domain.timetable.domain.model
+
+data class Timetable(
+    val id: Long? = null,
+    val grade: String? = null,
+    val classNum: String? = null,
+    val time: String? = null,
+    val subject: String? = null,
+)
