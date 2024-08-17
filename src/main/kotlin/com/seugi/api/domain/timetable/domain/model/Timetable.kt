7aeeee3c7f540492a97f015fcbf9c12e99bd5e05
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-17 21:05:19 +0900

    Fix :: 모델에 워크스페이스 아이디 추가

--- src/main/kotlin/com/seugi/api/domain/timetable/domain/model/Timetable.kt
+++ src/main/kotlin/com/seugi/api/domain/timetable/domain/model/Timetable.kt
@@ -2,6 +2,7 @@ package com.seugi.api.domain.timetable.domain.model
 
 data class Timetable(
     val id: Long? = null,
+    val workspaceId: String? = null,
     val grade: String? = null,
     val classNum: String? = null,
     val time: String? = null,
