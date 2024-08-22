91fc46c1185b44ef84cc789cd3527741ca66feb2
이슬아 <rheeseula@gmail.com>
2024-08-22 20:09:29 +0900

    Feat :: BaseResponse에 에러 응답용 생성자 추가
    Signed-off-by: 이슬아 <rheeseula@gmail.com>

--- src/main/kotlin/com/seugi/api/global/response/BaseResponse.kt
+++ src/main/kotlin/com/seugi/api/global/response/BaseResponse.kt
@@ -1,10 +1,11 @@
 package com.seugi.api.global.response
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.seugi.api.global.exception.CustomErrorCode
 import org.springframework.http.HttpStatus
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class BaseResponse<T> (
+data class BaseResponse<T>(
 
     val status: Int = HttpStatus.OK.value(),
     val success: Boolean = true,
@@ -12,5 +13,15 @@ data class BaseResponse<T> (
     val message: String,
     val data: T? = null
 
-)
+) {
+
+    // errorResponse constructor
+    constructor(code: CustomErrorCode) : this(
+        status = code.status.value(),
+        success = false,
+        state = code.state,
+        message = code.message
+    )
+
+}
 
