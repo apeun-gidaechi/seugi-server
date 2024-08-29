04dc94216de529bfbfb96b75944058ed5724d7be
이슬아 <rheeseula@gmail.com>
2024-08-29 16:51:24 +0900

    Fix :: JwtErrorCode JWT_TOKEN_EXPIRED FORBIDDEN에서 UNAUTHORIZED로 바꿈

--- src/main/kotlin/com/seugi/api/global/auth/jwt/exception/JwtErrorCode.kt
+++ src/main/kotlin/com/seugi/api/global/auth/jwt/exception/JwtErrorCode.kt
@@ -9,13 +9,12 @@ enum class JwtErrorCode (
     override val message: String,
 ): CustomErrorCode {
 
-    JWT_TOKEN_EXPIRED(HttpStatus.FORBIDDEN, "FORBIDDEN", "토큰이 만료되었어요"),
+    JWT_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "FORBIDDEN", "토큰이 만료되었어요"),
     JWT_TOKEN_SIGNATURE_ERROR(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "토큰의 서명이 일치하지 않아요"),
     JWT_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "토큰이 구조가 이상하거나 데이터가 일치하지 않아요"),
     JWT_TOKEN_UNSUPPORTED_ERROR(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "지원하지않는 토큰이에요"),
     JWT_TOKEN_ILL_EXCEPTION(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "JWT 처리 과정에서 오류가 발생했어요"),
     JWT_UNKNOWN_EXCEPTION(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "JWT 처리과정에서 알수없는 오류가 발생했어요"),
     JWT_MEMBER_NOT_MATCH(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "비밀번호가 일치하지 않아요"),
-    JWT_EMPTY_EXCEPTION(HttpStatus.FORBIDDEN, "FORBIDDEN", "토큰을 넣어주세요."),
 
 }
