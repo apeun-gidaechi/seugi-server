68d15f57cdf3c9cd3dd84b77f858b2793d1d4e6f
이슬아 <rheeseula@gmail.com>
2024-09-04 09:42:02 +0900

    Fix :: Member 도메인 Google 생성자 임시 삭제

--- src/main/kotlin/com/seugi/api/domain/member/application/model/Member.kt
+++ src/main/kotlin/com/seugi/api/domain/member/application/model/Member.kt
@@ -3,7 +3,6 @@ package com.seugi.api.domain.member.application.model
 import com.seugi.api.domain.member.adapter.`in`.dto.req.EditMemberRequest
 import com.seugi.api.domain.member.adapter.`in`.dto.req.RegisterMemberRequest
 import com.seugi.api.domain.member.application.model.value.*
-import com.seugi.api.global.infra.oauth.google.parse.GoogleParseClient
 import com.seugi.api.global.infra.oauth.google.parse.GoogleParseResponse
 
 data class Member (
@@ -26,17 +25,6 @@ data class Member (
         picture = MemberPicture(""),
         password = MemberPassword(encrypted),
         birth =  MemberBirth(""),
-        role = MemberRole("ROLE_USER"),
-        deleted = MemberDeleted(false)
-    )
-
-    constructor(dto: GoogleParseResponse): this (
-        id = MemberId(0),
-        name = MemberName(dto.name),
-        email = MemberEmail(dto.email),
-        picture = MemberPicture(""),
-        password = MemberPassword(""),
-        birth = MemberBirth(""),
         role = MemberRole("ROLE_USER"),
         deleted = MemberDeleted(false)
     )
