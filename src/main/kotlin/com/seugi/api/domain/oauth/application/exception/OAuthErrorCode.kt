fab21f4ca3c8b794142c468707a094156b12d3a0
이슬아 <rheeseula@gmail.com>
2024-09-24 17:21:40 +0900

    Feat :: OAuth 도메인 ErrorCode OAUTH_NOT_SUFFICIENT 추가

--- src/main/kotlin/com/seugi/api/domain/oauth/application/exception/OAuthErrorCode.kt
+++ src/main/kotlin/com/seugi/api/domain/oauth/application/exception/OAuthErrorCode.kt
@@ -10,6 +10,7 @@ enum class OAuthErrorCode (
 ) : CustomErrorCode {
 
     OAUTH_NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "연동이 발견되지 않았습니다"),
-    OAUTH_ALREADY_EXIST(HttpStatus.CONFLICT, "CONFLICT", "이미 연동되었습니다")
+    OAUTH_ALREADY_EXIST(HttpStatus.CONFLICT, "CONFLICT", "이미 연동되었습니다"),
+    OAUTH_NOT_SUFFICIENT(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "인증 정보가 불충분합니다")
 
 }
