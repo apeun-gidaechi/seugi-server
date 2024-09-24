2fd5351d511c67b9111551a2e1e0f391085f41e6
이슬아 <rheeseula@gmail.com>
2024-09-24 17:44:58 +0900

    Feat :: OAuth 도메인 AppleCodeRequest 추가

---
+++ src/main/kotlin/com/seugi/api/domain/oauth/adapter/in/dto/request/AppleCodeRequest.kt
@@ -0,0 +1,30 @@
+package com.seugi.api.domain.oauth.adapter.`in`.dto.request
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
+import com.seugi.api.global.common.enums.Platform
+
+class AppleCodeRequest (
+
+    @JsonProperty("code")
+    val code: String,
+
+    @JsonProperty("platform")
+    val platform: Platform,
+
+    @JsonProperty("name")
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    val name: String = "",
+
+    @JsonProperty("email")
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    val email: String = ""
+
+) {
+
+    fun isNameAndEmailEmpty(): Boolean {
+        return name.isEmpty() && email.isEmpty()
+    }
+
+}
