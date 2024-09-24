f55df9c956a3035ca498a3a72416cf5906a58a57
이슬아 <rheeseula@gmail.com>
2024-09-24 17:21:01 +0900

    Feat :: OAuth 도메인 Model sub 필드 추가

--- src/main/kotlin/com/seugi/api/domain/oauth/application/model/OAuth.kt
+++ src/main/kotlin/com/seugi/api/domain/oauth/application/model/OAuth.kt
@@ -2,24 +2,25 @@ package com.seugi.api.domain.oauth.application.model
 
 import com.seugi.api.domain.member.application.model.Member
 import com.seugi.api.domain.oauth.application.model.vo.*
-import com.seugi.api.global.infra.oauth.google.exchange.GoogleExchangeResponse
 
 class OAuth (
 
     val id: OAuthId,
     val member: Member,
     val provider: OAuthProvider,
+    val sub: OAuthSub,
     val accessToken: OAuthAccessToken,
     val refreshToken: OAuthRefreshToken
 
 ) {
 
-    constructor(dto: GoogleExchangeResponse, member: Member): this (
+    constructor(provider: String, sub: String, accessToken: String, refreshToken: String, member: Member): this (
         id = OAuthId(0),
         member = member,
-        provider = OAuthProvider("google"),
-        accessToken = OAuthAccessToken(dto.accessToken),
-        refreshToken = OAuthRefreshToken(dto.refreshToken)
+        provider = OAuthProvider(provider),
+        sub = OAuthSub(sub),
+        accessToken = OAuthAccessToken(accessToken),
+        refreshToken = OAuthRefreshToken(refreshToken)
     )
 
 }
