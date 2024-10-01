4f5625d5c5fdfbe5ee506b80a92207d407d1efb3
이슬아 <rheeseula@gmail.com>
2024-10-01 17:42:40 +0900

    Feat :: OAuth 도메인 Repository #existsBySubAndProvider 추가

--- src/main/kotlin/com/seugi/api/domain/oauth/adapter/out/repository/OAuthRepository.kt
+++ src/main/kotlin/com/seugi/api/domain/oauth/adapter/out/repository/OAuthRepository.kt
@@ -2,12 +2,14 @@ package com.seugi.api.domain.oauth.adapter.out.repository
 
 import com.seugi.api.domain.member.adapter.out.entity.MemberEntity
 import com.seugi.api.domain.oauth.adapter.out.entity.OAuthEntity
+import com.seugi.api.global.common.enums.Provider
 import org.springframework.data.repository.CrudRepository
 
 interface OAuthRepository : CrudRepository<OAuthEntity, Long>, OAuthCustomRepository {
 
-    override fun findByMemberAndProvider(member: MemberEntity, provider: String): OAuthEntity?
-    override fun findByProviderAndSub(provider: String, sub: String): OAuthEntity?
-    override fun existsByMemberAndProvider(member: MemberEntity, provider: String): Boolean
+    override fun findByMemberAndProvider(member: MemberEntity, provider: Provider): OAuthEntity?
+    override fun findByProviderAndSub(provider: Provider, sub: String): OAuthEntity?
+    override fun existsByMemberAndProvider(member: MemberEntity, provider: Provider): Boolean
+    override fun existsBySubAndProvider(sub: String, provider: Provider): Boolean
 
 }
