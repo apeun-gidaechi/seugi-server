33ee3551a479404857aac6042855c92d8461a11d
이슬아 <rheeseula@gmail.com>
2024-10-01 17:43:59 +0900

    Fix :: OAuth 도메인 Provider VO value 타입 Provider로 변경

--- src/main/kotlin/com/seugi/api/domain/oauth/application/model/vo/OAuthProvider.kt
+++ src/main/kotlin/com/seugi/api/domain/oauth/application/model/vo/OAuthProvider.kt
@@ -1,7 +1,9 @@
 package com.seugi.api.domain.oauth.application.model.vo
 
+import com.seugi.api.global.common.enums.Provider
+
 class OAuthProvider (
 
-    val value: String
+    val value: Provider
 
 )
