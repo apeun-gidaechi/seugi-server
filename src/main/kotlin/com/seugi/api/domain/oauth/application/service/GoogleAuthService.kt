7ce6029e712df4b303d2b1ac254c48b9ffab4b39
이슬아 <rheeseula@gmail.com>
2024-10-01 17:51:38 +0900

    Fix :: OAuth 도메인 Google Auth redirectUri, client 로직 분리

--- src/main/kotlin/com/seugi/api/domain/oauth/application/service/GoogleAuthService.kt
+++ src/main/kotlin/com/seugi/api/domain/oauth/application/service/GoogleAuthService.kt
@@ -1,18 +1,17 @@
 package com.seugi.api.domain.oauth.application.service
 
 import com.seugi.api.domain.member.application.model.Member
-import com.seugi.api.domain.member.application.port.out.ExistMemberPort
 import com.seugi.api.domain.member.application.port.out.LoadMemberPort
 import com.seugi.api.domain.member.application.port.out.SaveMemberPort
 import com.seugi.api.domain.oauth.adapter.`in`.dto.request.GoogleCodeRequest
 import com.seugi.api.domain.oauth.application.model.OAuth
 import com.seugi.api.domain.oauth.port.`in`.GoogleAuthUseCase
+import com.seugi.api.domain.oauth.port.out.ExistOAuthPort
 import com.seugi.api.domain.oauth.port.out.SaveOAuthPort
 import com.seugi.api.global.auth.jwt.JwtInfo
 import com.seugi.api.global.auth.jwt.JwtUtils
-import com.seugi.api.global.auth.oauth.google.GoogleProperties
-import com.seugi.api.global.infra.oauth.google.GoogleClient
-import com.seugi.api.global.infra.oauth.google.req.GoogleExchangeRequest
+import com.seugi.api.global.auth.oauth.google.GoogleUtils
+import com.seugi.api.global.common.enums.Provider
 import com.seugi.api.global.response.BaseResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -21,26 +20,34 @@ import java.nio.charset.StandardCharsets
 
 @Service
 class GoogleAuthService (
-    private val properties: GoogleProperties,
-    private val client: GoogleClient,
     private val saveOAuthPort: SaveOAuthPort,
-    private val existMemberPort: ExistMemberPort,
+    private val existOAuthPort: ExistOAuthPort,
     private val loadMemberPort: LoadMemberPort,
     private val saveMemberPort: SaveMemberPort,
     private val jwtUtils: JwtUtils,
+    private val googleUtils: GoogleUtils
 ) : GoogleAuthUseCase {
 
     @Transactional
     override fun authenticate(dto: GoogleCodeRequest): BaseResponse<JwtInfo> {
-        val decode = URLDecoder.decode(dto.code, StandardCharsets.UTF_8)
-        val exchange = client.exchange(GoogleExchangeRequest(decode, properties))
-        val parse = client.parse(exchange.idToken)
+        val redirectUri = googleUtils.getRedirectUri(dto.platform)
 
-        if (!existMemberPort.existMemberWithEmail(parse.email)) {
+        val decode = URLDecoder.decode(dto.code, StandardCharsets.UTF_8)
+        val exchange = googleUtils.exchange(decode, redirectUri)
+        val parse = googleUtils.parse(exchange.idToken)
+
+        if (!existOAuthPort.existOAuthBySubAndProvider(parse.sub, Provider.GOOGLE)) {
             val model = Member(parse.name, parse.email)
             val member = saveMemberPort.saveMember(model)
 
-            val oauth = OAuth("google", parse.sub, exchange.accessToken, exchange.refreshToken, member)
+            val oauth = OAuth(
+                Provider.GOOGLE,
+                parse.sub,
+                exchange.accessToken,
+                exchange.refreshToken!!,
+                member
+            )
+
             saveOAuthPort.saveOAuth(oauth)
 
             return BaseResponse (
