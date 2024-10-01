0574355c81d54d93461046d6b6e9b49da8a1dd1f
이슬아 <rheeseula@gmail.com>
2024-10-01 18:13:19 +0900

    Fix :: AppleExchangeResponse expiresIn 삭제, refreshToken nullable로 수정

--- src/main/kotlin/com/seugi/api/global/infra/oauth/apple/res/AppleExchangeResponse.kt
+++ src/main/kotlin/com/seugi/api/global/infra/oauth/apple/res/AppleExchangeResponse.kt
@@ -7,9 +7,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class AppleExchangeResponse (
 
     @JsonProperty("access_token") val accessToken: String,
-    @JsonProperty("refresh_token") val refreshToken: String,
+    @JsonProperty("refresh_token") val refreshToken: String?,
     @JsonProperty("id_token") val idToken: String,
-    @JsonProperty("expires_in") val expiresIn: Long,
     @JsonProperty("token_type") val tokenType: String
 
 )
