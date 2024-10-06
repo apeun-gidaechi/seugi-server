4a88bc2c6875dcfef4a4bd28be09cd5610c4149a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-10-06 21:24:27 +0900

    Fix :: Remove not use file #266

--- src/main/kotlin/com/seugi/api/domain/chat/domain/room/info/RoomInfoRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.seugi.api.domain.chat.domain.room.info
-
-import org.springframework.data.repository.CrudRepository
-
-interface RoomInfoRepository : CrudRepository<RoomInfoEntity, Long> {
-    fun findByRoomId(roomId: String): List<RoomInfoEntity>?
-}
