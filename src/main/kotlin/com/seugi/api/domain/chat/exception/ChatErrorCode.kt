4fd9482079daefc934eb306c65fdbde6b842ce65
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-10-14 16:10:29 +0900

    Fix :: 채팅방 타입 검증 로직 추가 #293
    채팅방 타입이 올바른지 검증하는 로직과 오류 Enum 을 추가하였습니다.

--- src/main/kotlin/com/seugi/api/domain/chat/exception/ChatErrorCode.kt
+++ src/main/kotlin/com/seugi/api/domain/chat/exception/ChatErrorCode.kt
@@ -10,6 +10,7 @@ enum class ChatErrorCode(
 ) : CustomErrorCode {
     CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "채팅방을 찾을 수 없습니다."),
     NO_ACCESS_ROOM(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "채팅방 권한이 없습니다."),
+    NO_TYPE_ROOM(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "채팅방 타입을 잘못 요청하셨습니다."),
     NO_ACCESS_MESSAGE(HttpStatus.FORBIDDEN, "FORBIDDEN", "메세지 삭제 권한이 없습니다."),
     CHAT_ROOM_CREATE_ERROR(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "채팅방 인원을 확인해주세요."),
     CHAT_ROOM_ID_ERROR(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "채팅방 ID를 확인해주세요."),
