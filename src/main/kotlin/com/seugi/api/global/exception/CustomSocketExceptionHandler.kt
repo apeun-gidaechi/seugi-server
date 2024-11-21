f9a7cad3822a8127e91580bfa170eca546591ab6
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-11-21 20:43:55 +0900

    Fix :: 소켓 예외 핸들러 수정 및 세션 제거 기능 추가
    SocketException 핸들러에 세션 제거 기능을 추가했습니다. 또한, runtime 예외 메시지를 표시하는 방식을 개선했습니다.

--- src/main/kotlin/com/seugi/api/global/exception/CustomSocketExceptionHandler.kt
+++ src/main/kotlin/com/seugi/api/global/exception/CustomSocketExceptionHandler.kt
@@ -2,45 +2,46 @@ package com.seugi.api.global.exception
 
 import com.seugi.api.global.response.ErrorResponse
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler
-import org.springframework.messaging.simp.SimpAttributesContextHolder
+import org.springframework.messaging.Message
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.web.bind.annotation.ControllerAdvice
+import java.io.IOException
 import java.net.SocketException
 import java.security.Principal
 
 @ControllerAdvice
 class CustomSocketExceptionHandler(
-//    private val template: RabbitMessagingTemplate
-    private val template: SimpMessagingTemplate,
+    private val template: SimpMessagingTemplate
 ) {
 
+    private val bindingUrl = "/queue/errors"
+
     @MessageExceptionHandler(SocketException::class)
-    fun handleSocketException(principal: Principal, ex: Exception) {
+    fun handleSocketException(message: Message<*>, principal: Principal, ex: Exception) {
+        removeSession(message)
         template.convertAndSendToUser(
             principal.name,
-            "/error",
+            bindingUrl,
             ErrorResponse(status = 4500, message = ex.cause?.message ?: "Socket Error")
         )
     }
 
     @MessageExceptionHandler(RuntimeException::class)
     fun handleRuntimeException(principal: Principal, ex: Exception) {
-        val simpAttributes = SimpAttributesContextHolder.currentAttributes()
-        println(
-            principal.name + " | " + simpAttributes.getAttribute("sub").toString() + " | " + simpAttributes.sessionId
-        )
         template.convertAndSendToUser(
             principal.name,
-            "/queue/errors",
-            ErrorResponse(status = 4500, message = ex.cause?.message ?: "Socket Error")
+            bindingUrl,
+            ErrorResponse(status = 4500, message = ex.cause?.stackTraceToString() ?: "Socket Error")
         )
     }
 
-//    @Throws(IOException::class)
-//    private fun removeSession(message: Message<*>) {
-//        val stompHeaderAccessor = StompHeaderAccessor.wrap(message)
-//        val sessionId = stompHeaderAccessor.sessionId
-//    }
+    @Throws(IOException::class)
+    private fun removeSession(message: Message<*>) {
+        val stompHeaderAccessor = StompHeaderAccessor.wrap(message)
+        val sessionId = stompHeaderAccessor.sessionId
+        stompHeaderAccessor.sessionAttributes?.remove(sessionId)
+    }
 
 
 }
